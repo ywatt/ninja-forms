dd6f90cf3cba270e9c3f580c648a0b9f975e5ae6
Kevin Stover <kstover@gmail.com>
2013-11-01 23:44:52 +0000

    Minifying modified display JS script.

--- includes/display/scripts.php
+++ includes/display/scripts.php
@@ -202,7 +202,7 @@ function ninja_forms_display_js($form_id, $local_vars = ''){
 	$msg_format = $plugin_settings['msg_format'];
 	$msg_format = 'inline';
 	wp_enqueue_script( 'ninja-forms-display',
-		NINJA_FORMS_URL .'/js/dev/ninja-forms-display.js',
+		NINJA_FORMS_URL .'/js/min/ninja-forms-display.min.js',
 		array( 'jquery', 'jquery-form' ) );
 
 	if( !isset( $ninja_forms_display_localize_js ) OR !$ninja_forms_display_localize_js ){
