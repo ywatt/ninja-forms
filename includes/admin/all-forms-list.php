6b96b5dfe40876304941ce31ffbaa576b04b0e77
Kevin Stover <kstover@gmail.com>
2013-12-22 16:06:29 +0000

    no message

--- includes/admin/all-forms-list.php
+++ includes/admin/all-forms-list.php
@@ -186,6 +186,7 @@ function ninja_forms_admin_all_forms() {
   	<div class="nf-new-form wp-core-ui media-frame" id="">
 		<div class="wizard-header">
 			<div alt="Close overlay" href="#close-modal" rel="modal:close" class="close dashicons dashicons-no"></div>
+			<div>Test</div>
 			<!--
 			<div alt="Show previous" class="left dashicons dashicons-no disabled"></div>
 			<div alt="Show next" class="right dashicons dashicons-no"></div>
