2849c1644f6a435799834195442605ea0e293836
Patrick Rauland <bftrick@gmail.com>
2014-01-14 12:39:33 -0700

    adding comments to CSV attachment function

--- includes/display/processing/attachment-test.php
+++ includes/display/processing/attachment-test.php
@@ -5,23 +5,42 @@ add_action( 'ninja_forms_update_sub', 'ninja_forms_attachment_test' );
 function ninja_forms_attachment_test( $sub_id ){
 	global $ninja_forms_processing;
 
-	if( $ninja_forms_processing->get_form_setting( 'admin_attach_csv' ) == 1 AND $ninja_forms_processing->get_action() == 'submit' ){
-		$files = $ninja_forms_processing->get_form_setting( 'admin_attachments' );
+	// make sure this form is supposed to attach a CSV
+	if( 1 == $ninja_forms_processing->get_form_setting( 'admin_attach_csv' ) AND 'submit' == $ninja_forms_processing->get_action() ) {
+		
+		// convert submission id to array
 		$sub_ids = array($sub_id);
-		$csv = ninja_forms_export_subs_to_csv( $sub_ids, true );
+		
+		// create CSV content
+		$csv_content = ninja_forms_export_subs_to_csv( $sub_ids, true );
+		
+		// create temporary file
 		$path = tempnam( get_temp_dir(), 'Sub' );
 		$temp_file = fopen( $path, 'r+' );
-		fwrite( $temp_file, $csv );
+		
+		// write to temp file
+		fwrite( $temp_file, $csv_content );
 		fclose( $temp_file );
+		
+		// find the directory we will be using for the final file
 		$path = pathinfo( $path );
 		$dir = $path['dirname'];
 		$basename = $path['basename'];
+		
+		// create name for file
 		$new_name = apply_filters( 'ninja_forms_submission_csv_name', 'ninja-forms-submission' );
-		if( file_exists( $dir.'/'.$new_name.'.csv' ) ){
+		
+		// remove a file if it already exists
+		if( file_exists( $dir.'/'.$new_name.'.csv' ) ) {
 			unlink( $dir.'/'.$new_name.'.csv' );
 		}
+		
+		// move file
 		rename( $dir.'/'.$basename, $dir.'/'.$new_name.'.csv' );
 		$file1 = $dir.'/'.$new_name.'.csv';
+		
+		// add new file to array of existing files
+		$files = $ninja_forms_processing->get_form_setting( 'admin_attachments' );
 		array_push( $files, $file1 );
 		$ninja_forms_processing->update_form_setting( 'admin_attachments', $files );
 	}
