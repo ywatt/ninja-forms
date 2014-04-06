cf716d508d7115257efe4f8356054f3ebe2948c7
Stuart Sequeira <stuart@lb3computingsolutions.com>
2014-04-05 21:03:49 -0700

    add $credit_card['expires_month'] and $credit_card['expires_month'] 	to $ninja_forms_processing->get_credit_card()
    modify $credit_card['expires'] - build value from new 'expires_month'
    	and 'expires_year' values
    
    This change is needed because the credit card field in
    	credit-card.php changed

--- includes/display/processing/class-ninja-forms-processing.php
+++ includes/display/processing/class-ninja-forms-processing.php
@@ -1160,7 +1160,12 @@ class Ninja_Forms_Processing {
 			$credit_card['number'] = $number;
 			$credit_card['cvc'] = $this->data['extra']['_credit_card_cvc'];
 			$credit_card['name'] = $this->data['extra']['_credit_card_name'];
-			$credit_card['expires'] = $this->data['extra']['_credit_card_expires'];
+			$credit_card['expires'] = $this->data['extra']['_credit_card_expires_month'] 
+				. '/' . $this->data['extra']['_credit_card_expires_year'];
+			
+			$credit_card['expires_month'] = $this->data['extra']['_credit_card_expires_month'];
+			$credit_card['expires_year'] = $this->data['extra']['_credit_card_expires_year'];
+			
 			return $credit_card;
 		}
 	}
