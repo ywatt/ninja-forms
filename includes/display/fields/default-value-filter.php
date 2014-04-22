57356930f3f72643b615213c28201ff92e6bace5
Kevin Stover <kstover@gmail.com>
2014-04-22 15:44:15 +0100

    Default Values should now process short codes.

--- includes/display/fields/default-value-filter.php
+++ includes/display/fields/default-value-filter.php
@@ -21,6 +21,7 @@ function ninja_forms_default_value_filter( $data, $field_id ) {
 
 	if ( $default_value === false and isset ( $data['default_value'] ) ) {
 		$default_value = $data['default_value'];
+		$default_value = do_shortcode( $default_value );
 	}
 
 	if ( isset ( $ninja_forms_fields[ $field_type ]['process_field'] ) and $ninja_forms_fields[ $field_type ]['process_field'] ) {
