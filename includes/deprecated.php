1b42fb805fe30c06e659ac75b03695c8badb39ba
Kevin Stover <kstover@gmail.com>
2014-07-03 11:24:17 +0100

    Created a deprecated.php file to house older, legacy functions.

---
+++ includes/deprecated.php
@@ -0,0 +1,15 @@
+<?php
+
+// Hook into our new save sub filter to add any deprecated filters
+function nf_old_save_sub_filter( $user_value, $field_id ) {
+	return apply_filters( 'ninja_forms_save_sub', $user_value, $field_id );
+}
+
+add_filter( 'nf_save_sub_user_value', 'nf_old_save_sub_filter', 10, 2 );
+
+// Hook into our new nf_save_sub action and add any actions hooked into our old action hooks.
+function nf_old_save_sub_action( $sub_id ) {
+	do_action( 'ninja_forms_insert_sub', $sub_id );
+}
+
+add_action( 'nf_save_sub', 'nf_old_save_sub_action' );
