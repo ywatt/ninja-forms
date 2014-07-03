a57439f87481c06bdb4b61f8d72339e7ea7adf66
Kevin Stover <kstover@gmail.com>
2014-07-03 11:24:36 +0100

    Added two new functions for interacting with fields and subs.

---
+++ includes/functions.php
@@ -0,0 +1,47 @@
+<?php
+
+function ninja_forms_return_echo($function_name){
+	$arguments = func_get_args();
+    array_shift($arguments); // We need to remove the first arg ($function_name)
+    ob_start();
+    call_user_func_array($function_name, $arguments);
+	$return = ob_get_clean();
+	return $return;
+}
+
+function ninja_forms_random_string($length = 10){
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $random_string = '';
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $random_string;
+}
+
+function ninja_forms_remove_from_array($arr, $key, $val, $within = FALSE) {
+    foreach ($arr as $i => $array)
+            if ($within && stripos($array[$key], $val) !== FALSE && (gettype($val) === gettype($array[$key])))
+                unset($arr[$i]);
+            elseif ($array[$key] === $val)
+                unset($arr[$i]);
+
+    return array_values($arr);
+}
+
+function ninja_forms_letters_to_numbers( $size ) {
+	$l		= substr( $size, -1 );
+	$ret	= substr( $size, 0, -1 );
+	switch( strtoupper( $l ) ) {
+		case 'P':
+			$ret *= 1024;
+		case 'T':
+			$ret *= 1024;
+		case 'G':
+			$ret *= 1024;
+		case 'M':
+			$ret *= 1024;
+		case 'K':
+			$ret *= 1024;
+	}
+	return $ret;
+}
