08fbd6ba767d716549f77edcbb483ab6ef003f3c
Kevin Stover <kstover@gmail.com>
2014-07-29 19:11:28 +0100

    Fixed a bug that caused the text elements to be output with encoded html tags rather than the tags themselves.

--- includes/fields/desc.php
+++ includes/fields/desc.php
@@ -66,6 +66,8 @@ function ninja_forms_field_desc_display( $field_id, $data ){
 		$default_value = '';
 	}
 
+	$default_value = html_entity_decode( $default_value );
+
 	$default_value = wpautop( $default_value );
 
 	if( isset( $data['display_style'] ) ){
