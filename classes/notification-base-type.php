be31dc0c857d031f22bc80007fafd16057afc083
Kevin Stover <kstover@gmail.com>
2014-09-10 09:46:37 -0400

    False or non-existent fields are now removed from the table of fields.

--- classes/notification-base-type.php
+++ classes/notification-base-type.php
@@ -55,5 +55,31 @@ class NF_Notification_Base_Type
 		// This space left intentionally blank
 		return $data;
 	}
+
+	/**
+	 * Remove fields from the generated table if their value is bool(false)
+	 * 
+	 * @access public
+	 * @since 2.8
+	 * @return string $table
+	 */
+	public function remove_empty_fields_from_table( $table ) {
+		global $ninja_forms_processing;
+
+		$doc = new DOMDocument();
+		$doc->loadHTML( $table );
+
+		foreach ( $ninja_forms_processing->get_all_fields() as $field_id => $user_value ) {
+			if ( ! $user_value ) {
+				$element = $doc->getElementById( 'ninja_forms_field_' . $field_id );
+				if ( is_object( $element ) )
+					$element->parentNode->removeChild( $element );
+			}
+		}
+
+		$table = $doc->saveHTML();
+
+		return $table;
+	}
 	
 }
