071f6c6260e5eb81539a9e59b7a57915c64c4060
James Laws <james@wpninjas.com>
2014-09-15 10:08:40 -0400

    Add class to enlarge the redirect URL textbox.

--- classes/notification-redirect.php
+++ classes/notification-redirect.php
@@ -21,7 +21,7 @@ class NF_Notification_Redirect extends NF_Notification_Base_Type
 
 	/**
 	 * Output our edit screen
-	 * 
+	 *
 	 * @access public
 	 * @since 2.8
 	 * @return void
@@ -30,14 +30,14 @@ class NF_Notification_Redirect extends NF_Notification_Base_Type
 		?>
 		<tr>
 			<th scope="row"><label for="settings-redirect_url"><?php _e( 'Url', 'ninja-forms' ); ?></label></th>
-			<td><input type="text" name="settings[redirect_url]" id="settings-redirect_url" value="<?php echo esc_attr( nf_get_object_meta_value( $id, 'redirect_url' ) ); ?>"/></td>
+			<td><input type="text" name="settings[redirect_url]" id="settings-redirect_url" value="<?php echo esc_attr( nf_get_object_meta_value( $id, 'redirect_url' ) ); ?>" class="regular-text"/></td>
 		</tr>
 		<?php
 	}
 
 	/**
 	 * Process our Redirect notification
-	 * 
+	 *
 	 * @access public
 	 * @since 2.8
 	 * @return void
