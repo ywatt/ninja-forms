a5d11c3e549c7fcc3b62febc6776188afbceb593
Kevin Stover <kstover@gmail.com>
2014-09-19 11:56:47 -0400

    Code cleanup.

--- includes/ninja-settings.php
+++ includes/ninja-settings.php
@@ -15,23 +15,6 @@ function nf_get_settings(){
   $settings['javascript_error'] = isset ( $settings['javascript_error'] ) ? $settings['javascript_error'] : __( 'You cannot submit the form without Javascript enabled.', 'ninja-forms' );
   $settings['invalid_email'] = isset ( $settings['invalid_email'] ) ? $settings['invalid_email'] : __( 'Please enter a valid email address.', 'ninja-forms' );
   $settings['process_label'] = isset ( $settings['process_label'] ) ? $settings['process_label'] : __( 'Processing', 'ninja-forms' );
-  $settings['login_link'] = isset ( $settings['login_link'] ) ? $settings['login_link'] : '';
-  $settings['username_label'] = isset ( $settings['username_label'] ) ? $settings['username_label'] : '';
-  $settings['reset_password'] = isset ( $settings['reset_password'] ) ? $settings['reset_password'] : '';
-  $settings['password_label'] = isset ( $settings['password_label'] ) ? $settings['password_label'] : '';
-  $settings['repassword_label'] = isset ( $settings['repassword_label'] ) ? $settings['repassword_label'] : '';
-  $settings['password_mismatch'] = isset ( $settings['password_mismatch'] ) ? $settings['password_mismatch'] : '';
-  $settings['login_button_label'] = isset ( $settings['login_button_label'] ) ? $settings['login_button_label'] : '';
-  $settings['cancel_button_label'] = isset ( $settings['cancel_button_label'] ) ? $settings['cancel_button_label'] : '';
-  $settings['login_error'] = isset ( $settings['login_error'] ) ? $settings['login_error'] : '';
-  $settings['register_link'] = isset ( $settings['register_link'] ) ? $settings['register_link'] : '';
-  $settings['email_label'] = isset ( $settings['email_label'] ) ? $settings['email_label'] : '';
-  $settings['register_button_label'] = isset ( $settings['register_button_label'] ) ? $settings['register_button_label'] : '';
-  $settings['register_error'] = isset ( $settings['register_error'] ) ? $settings['register_error'] : '';
-  $settings['register_spam_q'] = isset ( $settings['register_spam_q'] ) ? $settings['register_spam_q'] : '';
-  $settings['register_spam_a'] = isset ( $settings['register_spam_a'] ) ? $settings['register_spam_a'] : '';
-  $settings['register_spam_error'] = isset ( $settings['register_spam_error'] ) ? $settings['register_spam_error'] : '';
-
 
   $settings['date_format']           = apply_filters( 'ninja_forms_labels/date_format'           , $settings['date_format'] );
   $settings['currency_symbol']       = apply_filters( 'ninja_forms_labels/currency_symbol'       , $settings['currency_symbol'] );
@@ -45,22 +28,6 @@ function nf_get_settings(){
   $settings['javascript_error']      = apply_filters( 'ninja_forms_labels/javascript_error'      , $settings['javascript_error'] );
   $settings['invalid_email']         = apply_filters( 'ninja_forms_labels/invalid_email'         , $settings['invalid_email'] );
   $settings['process_label']         = apply_filters( 'ninja_forms_labels/process_label'         , $settings['process_label'] );
-  $settings['login_link']            = apply_filters( 'ninja_forms_labels/login_link'            , $settings['login_link'] );
-  $settings['username_label']        = apply_filters( 'ninja_forms_labels/username_label'        , $settings['username_label'] );
-  $settings['reset_password']        = apply_filters( 'ninja_forms_labels/reset_password'        , $settings['reset_password'] );
-  $settings['password_label']        = apply_filters( 'ninja_forms_labels/password_label'        , $settings['password_label'] );
-  $settings['repassword_label']      = apply_filters( 'ninja_forms_labels/repassword_label'      , $settings['repassword_label'] );
-  $settings['password_mismatch']     = apply_filters( 'ninja_forms_labels/password_mismatch'     , $settings['password_mismatch'] );
-  $settings['login_button_label']    = apply_filters( 'ninja_forms_labels/login_button_label'    , $settings['login_button_label'] );
-  $settings['cancel_button_label']   = apply_filters( 'ninja_forms_labels/cancel_button_label'   , $settings['cancel_button_label'] );
-  $settings['login_error']           = apply_filters( 'ninja_forms_labels/login_error'           , $settings['login_error'] );
-  $settings['register_link']         = apply_filters( 'ninja_forms_labels/register_link'         , $settings['register_link'] );
-  $settings['email_label']           = apply_filters( 'ninja_forms_labels/email_label'           , $settings['email_label'] );
-  $settings['register_button_label'] = apply_filters( 'ninja_forms_labels/register_button_label' , $settings['register_button_label'] );
-  $settings['register_error']        = apply_filters( 'ninja_forms_labels/register_error'        , $settings['register_error'] );
-  $settings['register_spam_q']       = apply_filters( 'ninja_forms_labels/register_spam_q'       , $settings['register_spam_q'] );
-  $settings['register_spam_a']       = apply_filters( 'ninja_forms_labels/register_spam_a'       , $settings['register_spam_a'] );
-  $settings['register_spam_error']   = apply_filters( 'ninja_forms_labels/register_spam_error'   , $settings['register_spam_error'] );
 
   return $settings;
 } // nf_get_settings
