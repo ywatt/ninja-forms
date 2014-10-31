9e9602c3dc12a33d0da42cc856a8611a3486374e
Kevin Stover <kstover@gmail.com>
2014-10-31 16:32:46 -0400

    The "Custom first option" setting for the country field should now work properly with required fields. Closes #363.

--- includes/fields/country.php
+++ includes/fields/country.php
@@ -568,7 +568,7 @@ function ninja_forms_field_country_display( $field_id, $data ) {
 	}
 
 	if ( isset ( $data['country_use_custom_first'] ) AND $data['country_use_custom_first'] == 1 ) {
-		$countries = array_merge( array( $country_custom_first => $country_custom_first ), $countries );
+		$countries = array_merge( array( $country_custom_first => '' ), $countries );
 	}
 
 	$field_class = ninja_forms_get_field_class( $field_id );
