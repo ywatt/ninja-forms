17c07768fa47ca7bd59ad86ddc8b1997231d350d
Kevin Stover <kstover@gmail.com>
2015-02-03 13:32:31 -0500

    Changing the "Builder" tab to "Build your form"

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -96,8 +96,9 @@ function ninja_forms_admin(){
 				}
 
 				if ( 'form_list' == $current_tab ) {
+					$builder_url = add_query_arg( array( 'form_id' => 'new', 'tab' => 'builder' ) );
 					?>
-					<h2><?php _e( 'Forms', 'ninja-forms' ); ?> <a href="#" class="add-new-h2"><?php _e( 'Add New', 'ninja-forms'); ?></a></h2>
+					<h2><?php _e( 'Forms', 'ninja-forms' ); ?> <a href="<?php echo $builder_url; ?>" class="add-new-h2"><?php _e( 'Add New', 'ninja-forms'); ?></a></h2>
 					<?php
 				} else {
 
