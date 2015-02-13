a442b234a970826ac603220414df553ffcf28d7f
Kevin Stover <kstover@gmail.com>
2015-02-12 22:47:18 -0500

    Converting old forms to the new nf_object database structure.

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -357,19 +357,33 @@ function nf_change_state_dropdown_fav() {
  */
 function nf_maybe_update_form_settings() {
 	global $wpdb;
+	// Grab all of our forms.
 	$all_forms = $wpdb->get_results( 'SELECT * FROM ' . NINJA_FORMS_TABLE_NAME, ARRAY_A );
 
 	foreach ( $all_forms as $form ) {
+		// Check to see if an object exists with our form id.
 		$type = nf_get_object_type( $form['id'] );
-		if ( 'form' != $type ) {
-			$settings = maybe_unserialize( $form['data'] );
-			$settings['date_updated'] = $form['date_updated'];
-			$form_id = nf_insert_object( 'form', $form['id'] );
-			foreach ( $settings as $meta_key => $value ) {
-				nf_update_object_meta( $form_id, $meta_key, $value );
-			}
-			$wpdb->query( 'DELETE FROM ' . NINJA_FORMS_TABLE_NAME . ' WHERE id = ' . $form_id );
+		if ( $type && 'form' != $type ) {
+			// We have an object with our form id.
+			// Change its id to the last available id.
+			// Get our last available id.
+			$row = $wpdb->get_row( "SHOW TABLE STATUS LIKE '" . NF_OBJECTS_TABLE_NAME . "'", ARRAY_A );
+			$next_id = $row['Auto_increment'];
+
+			// Replace all instances of the object ID with our new one.
+			$wpdb->update( NF_OBJECTS_TABLE_NAME, array( 'id' => $next_id ), array( 'id' => $form['id'] ) );
+			$wpdb->update( NF_OBJECT_META_TABLE_NAME, array( 'object_id' => $next_id ), array( 'object_id' => $form['id'] ) );
+			$wpdb->update( NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array( 'parent_id' => $next_id ), array( 'parent_id' => $form['id'] ) );
+			$wpdb->update( NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array( 'child_id' => $next_id ), array( 'child_id' => $form['id'] ) );
 		}
+
+		$settings = maybe_unserialize( $form['data'] );
+		$settings['date_updated'] = $form['date_updated'];
+		$form_id = nf_insert_object( 'form', $form['id'] );
+		foreach ( $settings as $meta_key => $value ) {
+			nf_update_object_meta( $form_id, $meta_key, $value );
+		}
+		$wpdb->query( 'DELETE FROM ' . NINJA_FORMS_TABLE_NAME . ' WHERE id = ' . $form_id );
 	}
 }
 
