26d8ab87b2676139c12938b67ba440e44810c9b0
Kyle B. Johnson <kyle@wpninjas.com>
2015-04-07 11:42:08 -0400

    check reset flag to detemine id preservation policy

--- includes/display/upgrade-functions.php
+++ includes/display/upgrade-functions.php
@@ -8,26 +8,36 @@
 function nf_29_update_form_settings( $form_id ) {
 	global $wpdb;
 
-	// Check to see if an object exists with our form id.
-	$type = nf_get_object_type( $form_id );
-	if ( $type && 'form' != $type ) {
-		// We have an object with our form id.
-		// Insert a new object.
-		$next_id = nf_insert_object( $type );
-		
-		// Replace all instances of the object ID with our new one.
-		$wpdb->update( NF_OBJECT_META_TABLE_NAME, array( 'object_id' => $next_id ), array( 'object_id' => $form_id ) );
-		$wpdb->update( NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array( 'parent_id' => $next_id ), array( 'parent_type' => $type, 'parent_id' => $form_id ) );
-		$wpdb->update( NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array( 'child_id' => $next_id ), array( 'child_type' => $type, 'child_id' => $form_id ) );
-	
-		// Delete the original object
-		$wpdb->query( 'DELETE FROM ' . NF_OBJECTS_TABLE_NAME . ' WHERE id = ' . $form_id );
-	}
+    // Check to see if an object exists with our form id.
+    $type = nf_get_object_type($form_id);
+    if ($type && 'form' != $type) {
+        // We have an object with our form id.
+        // Insert a new object.
+        $next_id = nf_insert_object($type);
+
+        // Replace all instances of the object ID with our new one.
+        $wpdb->update(NF_OBJECT_META_TABLE_NAME, array('object_id' => $next_id), array('object_id' => $form_id));
+        $wpdb->update(NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array('parent_id' => $next_id), array('parent_type' => $type, 'parent_id' => $form_id));
+        $wpdb->update(NF_OBJECT_RELATIONSHIPS_TABLE_NAME, array('child_id' => $next_id), array('child_type' => $type, 'child_id' => $form_id));
+
+        // Delete the original object
+        $wpdb->query('DELETE FROM ' . NF_OBJECTS_TABLE_NAME . ' WHERE id = ' . $form_id);
+    }
 
 	$form = $wpdb->get_row( 'SELECT * FROM ' . NINJA_FORMS_TABLE_NAME . ' WHERE id = ' . $form_id, ARRAY_A );
 	$settings = maybe_unserialize( $form['data'] );
 	$settings['date_updated'] = $form['date_updated'];
-	$f_id = nf_insert_object( 'form', $form['id'] );
+
+    // Check to see if the conversion has been reset
+    $is_reset = get_option( 'nf_converted_form_reset', 0 );
+    if ( $is_reset ) {
+        // Do not preserve form id
+        $f_id = nf_insert_object( 'form' );
+    } else {
+        // Preserve form id
+        $f_id = nf_insert_object( 'form', $form['id'] );
+    }
+
 	foreach ( $settings as $meta_key => $value ) {
 		nf_update_object_meta( $f_id, $meta_key, $value );
 	}
