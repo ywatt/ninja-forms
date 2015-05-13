9d644a39f5fdbfc87f281ad83c29b8fbf1de5db0
Kyle B. Johnson <kyle@wpninjas.com>
2015-05-13 14:31:21 -0400

    add gulpfile for minifying js

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp   = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+gulp.task('js', function () {
+    return gulp.src('assets/js/dev/nf-upgrade-handler.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js/min/')); //the destination folder
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('assets/js/dev/nf-upgrade-handler.js', ['js']);
+});
+
+// Default Task
+gulp.task('default', ['js', 'watch']);
+
+function swallowError (error) {
+    //If you want details of the error in the console
+    console.log(error.toString());
+    this.emit('end');
+}
