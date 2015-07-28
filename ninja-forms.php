296592b0b565640debdd0bc2ad31a17471caa3e7
Kevin Stover <kstover@gmail.com>
2015-07-28 12:14:04 -0400

    Moving the code for the "Add Form" button and modal.

--- ninja-forms.php
+++ ninja-forms.php
@@ -151,8 +151,8 @@ class Ninja_Forms {
         // Check and update our version number.
         self::$instance->update_version_number();
 
-        // Add a tinyMCE button to our post and page editor
-        add_filter( 'media_buttons_context', array( self::$instance, 'insert_form_tinymce_buttons' ) );
+        // Add our "Add Form" button and modal to the tinyMCE editor
+        self::$instance->add_form_button = new NF_Admin_AddFormModal();
 
         // Get our admin notices up and running.
         self::$instance->notices = new NF_Notices();
@@ -407,6 +407,8 @@ class Ninja_Forms {
         require_once( NF_PLUGIN_DIR . 'classes/notifications-table.php' );
         // Include our base notification type
         require_once( NF_PLUGIN_DIR . 'classes/notification-base-type.php' );
+        // Include add form button and modal
+        require_once( NF_PLUGIN_DIR . 'classes/add-form-modal.php' );
 
         if ( is_admin () ) {
             // Include our step processing stuff if we're in the admin.
@@ -751,110 +753,6 @@ class Ninja_Forms {
      */
     public function refresh_plugin_settings() {
         self::$instance->plugin_settings = self::$instance->get_plugin_settings();
-    }
-
-    /**
-     * Output our tinyMCE field buttons
-     *
-     * @access public
-     * @since 2.8
-     * @return void
-     */
-    public function insert_form_tinymce_buttons( $context ) {
-        global $pagenow;
-
-        if ( 'post.php' != $pagenow ) {
-            return $context;
-        }
-        $html = '<style>
-            span.nf-insert-form {
-                color:#888;
-                font: 400 18px/1 dashicons;
-                -webkit-font-smoothing: antialiased;
-                -moz-osx-font-smoothing: grayscale;
-                display: inline-block;
-                width: 18px;
-                height: 18px;
-                vertical-align: text-top;
-                margin: 0 2px 0 0;
-            }
-        </style>';
-        $html .= '<a href="#" class="button-secondary nf-insert-form"><span class="nf-insert-form dashicons dashicons-feedback"></span> ' . __( 'Add Form', 'ninja-forms' ) . '</a>';
-        ninja_forms_admin_css();
-        ninja_forms_admin_js();
-
-        wp_enqueue_script( 'nf-combobox',
-            NF_PLUGIN_URL . 'assets/js/min/combobox.min.js',
-            array( 'jquery', 'jquery-ui-core', 'jquery-ui-button', 'jquery-ui-autocomplete' ) );
-        wp_enqueue_style( 'nf-combobox',
-            NF_PLUGIN_URL . 'assets/css/combobox.css' );
-
-        add_action( 'admin_footer', array( self::$instance, 'output_tinymce_button_js' ) );
-        return $html;
-    }
-
-    /**
-     * Output our tinyMCE field buttons
-     *
-     * @access public
-     * @since 2.8
-     * @return void
-     */
-    public function output_tinymce_button_js( $context ) {
-        ?>
-        <div id="nf-insert-form-modal" style="height:350px;">
-            <p>
-                <?php
-                $all_forms = Ninja_Forms()->forms()->get_all();
-                $first_option = __( 'Select a form or type to search', 'ninja-forms' );
-                echo '<select class="nf-forms-combobox" id="nf_form_select" data-first-option="' . $first_option . '">';
-                echo '<option value="">' . $first_option .'</option>';
-                foreach( $all_forms as $form_id ) {
-                    $label = esc_html( Ninja_Forms()->form( $form_id )->get_setting( 'form_title' ) );
-                    if ( strlen( $label ) > 30 )
-                        $label = substr( $label, 0, 30 ) . '...';
-
-                    echo '<option value="' . $form_id . '">' . $label . ' - ID: ' . $form_id . '</option>';
-                }
-                echo '</select>';
-                ?>
-            </p>
-        </div>
-
-        <div id="nf-insert-form-buttons">
-            <div id="nf-admin-modal-cancel">
-                <a class="submitdelete deletion modal-close" href="#"><?php _e( 'Cancel', 'ninja-forms' ); ?></a>
-            </div>
-            <div id="nf-admin-modal-update">
-                <a class="button-primary" id="nf-insert-form" href="#"><?php _e( 'Insert', 'ninja-forms' ); ?></a>
-            </div>
-        </div>
-
-        <script type="text/javascript">
-        jQuery( document ).ready( function( $ ) {
-
-            $( '#nf-insert-form-modal' ).nfAdminModal( { title: 'Test', buttons: '#nf-insert-form-buttons', backgroundClose: true } );
-            $( document ).on( 'click', '.nf-insert-form', function( e ) {
-                e.preventDefault();
-                $( '#nf-insert-form-modal' ).nfAdminModal( 'open' );
-                $( '.nf-forms-combobox' ).combobox();
-                $( '#nf-admin-modal-content .ui-autocomplete-input' ).focus().select();
-            } );
-
-            $( document ).on( 'click', '#nf-insert-form', function( e ) {
-                e.preventDefault();
-                var form_id = $( this ).parent().parent().parent().find( '#nf_form_select' ).val();
-                var shortcode = '[ninja_form id=' + form_id + ']';
-                window.parent.send_to_editor( shortcode );
-                $.fn.nfAdminModal.close();
-            } );
-
-            $( document ).on( 'nfAdminModalClose.destroyCombo', function( e ) {
-                $( '.nf-forms-combobox' ).combobox( 'destroy'  );
-            } );
-        } );
-        </script>
-        <?php
     }
 
 } // End Class
