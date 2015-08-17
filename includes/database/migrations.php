2a6c218029ae5e1e585e6f456207f68d3243ba18
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-17 15:46:43 -0400

    register migartions with handler

--- includes/database/migrations.php
+++ includes/database/migrations.php
@@ -2,8 +2,20 @@
 
 class NF_Database_Migrations
 {
+    private $migrations = array();
+
     public function __construct()
     {
+        $this->migrations[ 'forms' ]   = new NF_Database_Migrations_Forms();
+        $this->migrations[ 'fields' ]  = new NF_Database_Migrations_Fields();
+        $this->migrations[ 'actions' ] = new NF_Database_Migrations_Actions();
+    }
+
+    public function migrate()
+    {
+        foreach( $this->migrations as $migration ){
+            $migration->_run();
+        }
     }
 
 }
