1f790f91cc397c249ed4b430a288742721c147e7
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-17 15:51:49 -0400

    update form meta table columns.

--- includes/database/migrations/forms-meta.php
+++ includes/database/migrations/forms-meta.php
@@ -15,11 +15,8 @@ class NF_Database_Migrations_FormsMeta extends NF_Database_Migration
         $query = "CREATE TABLE IF NOT EXISTS $this->table_name (
             `id` int NOT NULL AUTO_INCREMENT,
             `form_id` int NOT NULL,
-            `title` tinytext,
-            `created_at` TIMESTAMP,
-            `updated_at` DATETIME,
-            `views` int(11),
-            `subs` int(11),
+            `key` tinytext NOT NULL,
+            `value` tinytext,
             UNIQUE KEY (`id`)
         ) $this->charset_collate;";
 
