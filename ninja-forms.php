32267e38248f0f232dc82ac69a7c49db8bf6f9ee
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-18 12:29:03 -0400

    add Ninja_Forms class with Autoloader. Closes #861 .

--- ninja-forms.php
+++ ninja-forms.php
@@ -13,4 +13,44 @@ Domain Path: /lang/
 Copyright 2015 WP Ninjas.
 */
 
-include 'deprecated/ninja-forms.php';
+define( 'LOAD_DEPRECATED', FALSE );
+
+if( defined( 'LOAD_DEPRECATED') AND LOAD_DEPRECATED ) {
+    include 'deprecated/ninja-forms.php';
+    return;
+}
+
+final class Ninja_Forms
+{
+    private static $instance;
+
+    public static function instance()
+    {
+        if (!isset(self::$instance) && !(self::$instance instanceof Ninja_Forms)) {
+            self::$instance = new Ninja_Forms;
+            spl_autoload_register(array(self::$instance, 'autoloader'));
+        }
+
+        return self::$instance;
+    }
+
+    public function autoloader($class_name)
+    {
+        if (false !== strpos($class_name, 'NF_')) {
+            $class_name = str_replace('NF_', '', $class_name);
+            $classes_dir = realpath(plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR;
+            $class_file = str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+            if (file_exists($classes_dir . $class_file)) {
+                require_once $classes_dir . $class_file;
+            }
+        }
+    }
+
+} // End Class Ninja_Forms
+
+function Ninja_Forms()
+{
+    return Ninja_Forms::instance();
+}
+
+Ninja_Forms();
