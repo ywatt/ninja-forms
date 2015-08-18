407bc80d88ff532777b97348596d1e1523e7b676
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-18 14:58:54 -0400

    add back stubborn files with renaming.

---
+++ includes/Database/Migrations/Actions.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+class NF_Database_Migrations_Actions extends NF_Database_Migration
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'nf_actions',
+            'nf_migration_create_table_actions'
+        );
+    }
+
+    public function run()
+    {
+        $query = "CREATE TABLE IF NOT EXISTS $this->table_name (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `title` tinytext,
+            `created_at` TIMESTAMP,
+            `updated_at` DATETIME,
+            UNIQUE KEY (`id`)
+        ) $this->charset_collate;";
+
+        dbDelta( $query );
+    }
+
+}
---
+++ includes/Database/Migrations/Forms.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+class NF_Database_Migrations_Forms extends NF_Database_Migration
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'nf_forms',
+            'nf_migration_create_table_forms'
+        );
+    }
+
+    public function run()
+    {
+        $query = "CREATE TABLE IF NOT EXISTS $this->table_name (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `title` tinytext,
+            `created_at` TIMESTAMP,
+            `updated_at` DATETIME,
+            `views` int(11),
+            `subs` int(11),
+            UNIQUE KEY (`id`)
+        ) $this->charset_collate;";
+
+        dbDelta( $query );
+    }
+
+}
