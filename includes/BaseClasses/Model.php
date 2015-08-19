18add5659a851db9732c06c51b520d343e07befb
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-19 10:46:05 -0400

    update methods for split tables and efficiency.

--- includes/BaseClasses/Model.php
+++ includes/BaseClasses/Model.php
@@ -2,16 +2,29 @@
 
 abstract class NF_BaseClasses_Model
 {
+    public $id = '';
+
     public $table_name = '';
 
     public $meta_table_name = '';
+
+    public $columns = array();
 
     public $settings = array();
 
     public function __construct( $id )
     {
+        global $wpdb;
+
         $this->id = $id;
+
+        $this->table_name      = $wpdb->prefix . $this->table_name;
+        $this->meta_table_name = $wpdb->prefix . $this->meta_table_name;
     }
+
+    /*
+     * PUBLIC METHODS
+     */
 
     public function update_setting( $key, $value )
     {
@@ -19,13 +32,27 @@ abstract class NF_BaseClasses_Model
 
         if( ! $this->id ) return FALSE;
 
+        if( in_array( $key, $this->columns ) ){
+
+            return $wpdb->update(
+                $this->table_name,
+                array(
+                    $key => $value
+                ),
+                array(
+                    'id' => $this->id
+                )
+            );
+        }
+
         return $wpdb->update(
             $this->meta_table_name,
             array(
-                $key => $value
+                'value' => $value
             ),
             array(
-                'ID' => $this->id
+                'id' => $this->id,
+                'key' => $key
             )
         );
     }
@@ -36,14 +63,36 @@ abstract class NF_BaseClasses_Model
 
         if( ! $this->id ) return FALSE;
 
-        return $wpdb->update(
-            $this->meta_table_name,
-            $data,
+        // Separate out columns from meta
+        $columns = array();
+
+        // Log results for single return
+        $results = array();
+
+        foreach( $data as $key => $value ){
+
+            if( in_array( $key, $this->columns ) ){
+                $columns[ $key ] = $value;
+                unset( $data[ $key ] );
+            } else {
+                $results[] = $this->update_setting($key, $value);
+            }
+        }
+
+        $results[] = $wpdb->update(
+            $this->table_name,
+            $columns,
             array(
-                'ID' => $this->id
+                'id' => $this->id
             )
         );
+
+        return in_array( FALSE, $results );
     }
+
+    /*
+     * STATIC METHODS
+     */
 
     public static function get( $id )
     {
