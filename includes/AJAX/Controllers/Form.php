0307d0e4b6dd9e4b15ba8b9cd7b08265734b0958
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-25 13:11:36 -0400

    stub form controller response

--- includes/AJAX/Controllers/Form.php
+++ includes/AJAX/Controllers/Form.php
@@ -1,7 +1,9 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;
 
-class NF_AJAX_Controllers_Form
+class NF_AJAX_Controllers_Form extends NF_BaseClasses_Controller
 {
+    public $form = '';
+
     public function __construct()
     {
         add_action( 'wp_ajax_nf_save_form',   array( $this, 'save' )   );
@@ -10,11 +12,46 @@ class NF_AJAX_Controllers_Form
 
     public function save()
     {
+        //TODO: Abstract
+        if( isset( $_POST['nf_form'] ) ) {
+            $this->form = $_POST['nf_form'];
+        } else {
+            json_encode( array( 'error' => 'No Form Object Found' ) );
+            wp_die();
+        }
 
+        $form_model = new NF_Database_Models_Form( 1 );
+
+        $result = $form_model->update_settings( $this->form['form_settings'] );
+
+        if ( false === $result ) $this->errors[] = $result;
+
+        $response = array( 'errors' => $this->errors, 'settings' => $this->form['form_settings'] );
+
+        echo json_encode( $response );
+
+        wp_die(); // this is required to terminate immediately and return a proper response
     }
 
     public function delete()
     {
+        //TODO: Abstract
+        if( isset( $_POST['nf_form'] ) ) {
+            $this->form = $_POST['nf_form'];
+        } else {
+            json_encode( array( 'error' => 'No Form Object Found' ) );
+            wp_die();
+        }
 
+        $form = new NF_Database_Models_Form( 1 );
+        $result = $form->delete();
+
+        if ( false === $result ) $this->errors[] = $result;
+
+        $response = array( 'errors' => $this->errors, 'settings' => $this->form['form_settings'] );
+
+        echo json_encode( $response );
+
+        wp_die(); // this is required to terminate immediately and return a proper response
     }
 }
