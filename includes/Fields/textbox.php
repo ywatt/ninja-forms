f6c89349c7eb34b21b8485858b481eb503e621f7
Kyle B. Johnson <kyle@wpninjas.com>
2015-08-31 16:17:36 -0400

    add type property and stub the template method

--- includes/Fields/textbox.php
+++ includes/Fields/textbox.php
@@ -2,13 +2,23 @@
 
 final class NF_Field_Textbox extends NF_Abstracts_Field
 {
-  protected $_name = 'textbox';
+    protected $_name = 'textbox';
 
-  protected $_group = 'standard_fields';
+    protected $_group = 'standard_fields';
 
-  public function __construct()
-  {
+    protected $_type = 'text';
 
-  }
+    public function template()
+    {
+        // Placeholder output
+        ?>
+        <input type="<?php echo $this->type; ?>">
+        <?php
+    }
+
+    public function validate( $value )
+    {
+        parent::validate( $value );
+    }
 
 }
