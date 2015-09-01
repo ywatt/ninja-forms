abdadb514c1f78057fd4b4513e6484d7468520a0
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-01 10:25:28 -0400

    fix merge conflicts

--- includes/Abstracts/Field.php
+++ includes/Abstracts/Field.php
@@ -6,7 +6,6 @@
 abstract class NF_Abstracts_Field
 {
     /**
-<<<<<<< HEAD
     * @var string
     */
     protected $_name  = '';
@@ -27,67 +26,26 @@ abstract class NF_Abstracts_Field
     protected $_test_value = '';
 
     /**
-    * @var string
-    */
+     * @var string
+     */
     protected $_attr = '';
-
-    /**
-    * Constructor
-    */
-=======
-     * @var string
-     */
-    protected $_name  = '';
-
-    /**
-     * @var string
-     */
-    protected $_group = '';
-
-    /**
-     * @var array
-     */
-    protected $_settings = array();
-
-    /**
-     * @var string
-     */
-    protected $_test_value = '';
-
-    /**
-     * @var string
-     */
-     protected $_attr = '';
 
     /**
      * Constructor
      */
->>>>>>> f6c89349c7eb34b21b8485858b481eb503e621f7
     public function __construct()
     {
 
     }
 
     /*
-<<<<<<< HEAD
     * PUBLIC METHODS
     */
-=======
-     * PUBLIC METHODS
-     */
->>>>>>> f6c89349c7eb34b21b8485858b481eb503e621f7
-    public function register()
+    function register()
     {
 
     }
 
-<<<<<<< HEAD
-    public abstract function template();
-
-    public abstract function validate();
-
-    public abstract function process();
-=======
     public function process()
     {
         // This section intentionally left blank.
@@ -99,6 +57,6 @@ abstract class NF_Abstracts_Field
     }
 
     public abstract function template();
->>>>>>> f6c89349c7eb34b21b8485858b481eb503e621f7
+
 
 }
