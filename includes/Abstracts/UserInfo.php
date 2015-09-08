62dbdea56ef6c810f8ed7238475b62da711117b5
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-08 14:43:56 -0400

    update config file name

--- includes/Abstracts/UserInfo.php
+++ includes/Abstracts/UserInfo.php
@@ -15,7 +15,7 @@ abstract class NF_Abstracts_UserInfo extends NF_Abstracts_Input
     {
         parent::__construct();
 
-        $settings = Ninja_Forms::config( 'UserInfoSettings' );
+        $settings = Ninja_Forms::config( 'UserInfoFieldSettings' );
 
         $this->_settings = array_merge( $this->_settings, $settings );
     }
