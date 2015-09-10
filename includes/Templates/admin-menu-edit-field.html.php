bdc5187c2481ca48212fc4d273baf7f76db124a9
James Laws <james@wpninjas.com>
2015-09-09 22:44:00 -0400

    Implemented SASS for handling all CSS and built the initial structure for the UI. Specifically the Header and editing fields.

--- includes/Templates/admin-menu-edit-field.html.php
+++ includes/Templates/admin-menu-edit-field.html.php
@@ -1,34 +1,58 @@
-<div id="builder">
+<script>
+jQuery(document).ready(function($){
+    $(".nf-item-edit").click(function(){
+        $("#nf-builder").removeClass("nf-drawer-closed");
+        $("#nf-builder").addClass("nf-drawer-opened");
+    });
+    $(".nf-close-drawer").click(function(){
+        $("#nf-builder").removeClass("nf-drawer-opened");
+        $("#nf-builder").addClass("nf-drawer-closed");
+    });
+});
+</script>
+<div id="nf-builder">
+    <div id="nf-header">
+        <div id="nf-main-header">
+            <div id="nf-logo"></div>
+            <ul>
+                <li><a class="active" href="#">Form Fields</a></li>
+                <li><a href="#">Emails & Actions</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a class="preview" href="#">Live Preview<span class="dashicons dashicons-visibility"></span></a></li>
+            </ul>
+            <input class="nf-button primary" type="submit" value="Save Changes" />
+            <a class="nf-cancel" href="#">Cancel</a>
+        </div>
+
+        <div id="nf-sub-header">
+            <!-- <h2>Contact Form</h2> -->
+            <a class="nf-add-new" href="#">Add new field</a>
+            <!-- <input class="nf-button secondary" type="submit" value="Edit Emails and Actions" /> -->
+
+        </div>
+
+    </div>
+
     <div id="nf-main">
         <!-- main content area. Where fields and actions are rendered. -->
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
-        <div class="nf-field-wrap">Field</div>
+        <div id="nf-main-controls">
+            <h2>Contact Form</h2>
+            <input class="nf-button secondary" type="submit" value="Edit Emails and Actions" />
+        </div>
+        <div id="nf-form-wrap">
+
+<?php
+for ($i=0; $i < 25; $i++) {
+    echo '<div class="nf-field-wrap">Field
+        <ul class="nf-item-controls">
+            <li class="nf-item-delete"><a href="#"><span class="dashicons dashicons-dismiss"></span><span class="nf-tooltip">Delete</span></a></li>
+            <li class="nf-item-duplicate"><a href="#"><span class="dashicons dashicons-admin-page"></span><span class="nf-tooltip">Duplicate</span></a></li>
+            <li class="nf-item-edit"><a href="#"><span class="dashicons dashicons-admin-generic"></span><span class="nf-tooltip">Edit</span></a></li>
+        </ul>
+    </div>';
+}
+?>
+        </div>
     </div>
 
     <div id="nf-drawer">
@@ -36,7 +60,7 @@
         <!-- THIS IS THE CONTENT FOR EDITING FIELDS -->
         <header class="nf-full">
             <h2>Editing Field</h2>
-            <span><input type="submit" class="save-field-settings" value="SAVE" /></span>
+            <input type="submit" class="nf-button primary nf-close-drawer" value="Close" />
         </header>
         <div class="nf-one-half">
             <label>Label Name</label>
