068695f21d89c002b49c88bcc96cd5f635e121d8
James Laws <james@wpninjas.com>
2015-09-11 13:07:28 -0400

    Made some additional adjustments to SASS files and markup.

--- includes/Templates/admin-menu-add-action.html.php
+++ includes/Templates/admin-menu-add-action.html.php
@@ -4,11 +4,13 @@ jQuery(document).ready(function($){
         $("#nf-builder").removeClass("nf-drawer-closed");
         $("#nf-builder").addClass("nf-drawer-opened");
         //$(".nf-field-wrap:first-child").addClass("active");
+        //$("#action-0 .nf-item-controls").addClass("nf-editing");
     });
     $(".nf-close-drawer").click(function(){
         $("#nf-builder").removeClass("nf-drawer-opened");
         $("#nf-builder").addClass("nf-drawer-closed");
         //$(".nf-field-wrap:first-child").removeClass("active");
+        //$("#action-0 .nf-item-controls").removeClass("nf-editing");
     });
 });
 </script>
@@ -38,7 +40,7 @@ jQuery(document).ready(function($){
     echo '<ul class="nf-item-controls">
         <li class="nf-item-delete"><a href="#"><span class="dashicons dashicons-dismiss"></span><span class="nf-tooltip">Delete</span></a></li>
         <li class="nf-item-duplicate"><a href="#"><span class="dashicons dashicons-admin-page"></span><span class="nf-tooltip">Duplicate</span></a></li>
-        <li class="nf-item-edit"><a href="#"><span class="dashicons dashicons-admin-generic"></span><span class="nf-tooltip">Edit</span><span class="nf-editing">Editing field</span></a></li>
+        <li class="nf-item-edit"><a href="#"><span class="dashicons dashicons-admin-generic"></span><span class="nf-tooltip">Edit</span><span class="nf-item-editing">Editing field</span></a></li>
     </ul>';
 } ?>
     <div id="nf-main">
