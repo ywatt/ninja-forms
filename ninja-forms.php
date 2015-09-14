ce83fc90056aea71bf993e0d9ccee6dc6eb8eebc
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-14 14:16:46 -0400

    remove action controller

--- ninja-forms.php
+++ ninja-forms.php
@@ -139,7 +139,6 @@ if( defined( 'LOAD_DEPRECATED') AND LOAD_DEPRECATED ) {
                  * AJAX Controllers
                  */
                 self::$instance->controllers[ 'form' ]       = new NF_AJAX_Controllers_Form();
-                self::$instance->controllers[ 'action' ]     = new NF_AJAX_Controllers_Action();
                 self::$instance->controllers[ 'submission' ] = new NF_AJAX_Controllers_Submission();
 
                 /*
