30993fc4677a902ff2b34509c880bf391ce098ce
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-14 14:21:57 -0400

    add response properties and method to base class

--- includes/Abstracts/Controller.php
+++ includes/Abstracts/Controller.php
@@ -2,11 +2,21 @@
 
 abstract class NF_Abstracts_Controller
 {
-    public $errors = array();
+    protected $_errors = array();
+
+    protected $_data = array();
 
     public function __construct()
     {
         // TODO: Check Nonce
     }
 
+    protected function _respond()
+    {
+        $response = array( 'errors' => $this->_errors, 'data' => $this->_data );
+
+        echo json_encode( $response );
+
+        wp_die(); // this is required to terminate immediately and return a proper response
+    }
 }
