5141e5af218ca5aa306915d6e527b340d14ddd40
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-21 09:49:51 -0400

    add helper getter and helper methods for timing, priority, and sorting

--- includes/Abstracts/Action.php
+++ includes/Abstracts/Action.php
@@ -46,4 +46,32 @@ abstract class NF_Abstracts_Action
      * Process
      */
     public abstract function process( $action_id, $form_id, $data );
+
+    public function get_timing()
+    {
+        $timing = array( 'early' => '1', 'normal' => '0', 'late' => '-1' );
+
+        return $timing[ $this->_timing ];
+    }
+
+    public function get_priority()
+    {
+        return $this->_priority;
+    }
+
+    public static function sort_actions( $a, $b )
+    {
+        $a->timing   = Ninja_Forms()->actions[ $a->get_setting( 'type' ) ]->get_timing();
+        $a->priority = Ninja_Forms()->actions[ $a->get_setting( 'type' ) ]->get_priority();
+
+        $b->timing   = Ninja_Forms()->actions[ $b->get_setting( 'type' ) ]->get_timing();
+        $b->priority = Ninja_Forms()->actions[ $b->get_setting( 'type' ) ]->get_priority();
+
+        // Compare Priority if Timing is the same
+        if( $a->timing == $b->timing)
+            return $a->priority > $b->priority ? 1 : -1;
+
+        // Compare Timing
+        return $a->timing < $b->timing ? 1 : -1;
+    }
 }
