c9c3163ec502cacc2e3269ad5850a882380e34d2
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-25 14:38:24 -0400

    stub out the uploads controller

---
+++ includes/AJAX/Controllers/Uploads.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+class NF_AJAX_Controllers_Uploads extends NF_Abstracts_Controller
+{
+    public function __construct()
+    {
+        add_action( 'wp_ajax_nf_async_upload', array( $this, 'upload' ) );
+        add_action( 'wp_ajax_nopriv_nf_async_upload', array( $this, 'upload' ) );
+    }
+
+    public function upload()
+    {
+
+    }
+}
