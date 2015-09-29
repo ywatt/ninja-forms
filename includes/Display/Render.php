5a1b780d57bb4cadb91ee3013d5cbed80347996f
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-29 10:31:40 -0400

    add late priority

--- includes/Display/Render.php
+++ includes/Display/Render.php
@@ -6,8 +6,8 @@ final class NF_Display_Render
 
     public static function localize( $form_id )
     {
-        if( ! has_action( 'wp_footer', 'NF_Display_Render::output_templates' ) ){
-            add_action( 'wp_footer', 'NF_Display_Render::output_templates' );
+        if( ! has_action( 'wp_footer', 'NF_Display_Render::output_templates', 9999 ) ){
+            add_action( 'wp_footer', 'NF_Display_Render::output_templates', 9999 );
         }
         $form = Ninja_Forms()->form( $form_id )->get();
 
