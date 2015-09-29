ad02e331436d43793db4e3ab9d26ec2fd806fab5
Kyle B. Johnson <kyle@wpninjas.com>
2015-09-29 11:53:12 -0400

    add test form for uploads controller

--- includes/Display/Shortcodes/tmp-file-upload.php
+++ includes/Display/Shortcodes/tmp-file-upload.php
@@ -2,11 +2,13 @@
 
 function nf_tmp_file_upload() {
 
-    $file = array_values($_FILES)[0];
+    if( $_FILES ) {
+        $file = array_values($_FILES)[0];
 
-    echo "<pre>";
-    var_dump($file);
-    echo "</pre>";
+        echo "<pre>";
+        var_dump($file);
+        echo "</pre>";
+    }
     ?>
 
     <form id="theForm" action="" method="POST" enctype="multipart/form-data">
@@ -14,18 +16,77 @@ function nf_tmp_file_upload() {
         <input type="submit">
     </form>
 
-    <!--
     <script>
         jQuery(document).ready(function($) {
-			var ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
-			var data = {
-				action: 'nf_async_upload',
-			};
 
-            $("#theForm").ajaxForm({url: 'ajaxurl', type: 'post', data: data } );
+            // Variable to store your files
+            var files;
+
+            // Add events
+            $('input[type=file]').on('change', prepareUpload);
+
+            // Grab the files and set them to our variable
+            function prepareUpload(event)
+            {
+                files = event.target.files;
+            }
+
+            var ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
+
+            $('form').on('submit', uploadFiles);
+
+            // Catch the form submit and upload the files
+            function uploadFiles(event)
+            {
+                event.stopPropagation(); // Stop stuff happening
+                event.preventDefault(); // Totally stop stuff happening
+
+                // START A LOADING SPINNER HERE
+
+                // Create a formdata object and add the files
+                var data = new FormData();
+                $.each(files, function(key, value)
+                {
+                    data.append(key, value);
+                });
+
+                data.append( 'action', 'nf_async_upload' );
+
+                $.ajax({
+                    url: ajaxurl,
+                    type: 'POST',
+                    data: data,
+                    cache: false,
+                    dataType: 'json',
+                    processData: false, // Don't process the files
+                    contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+                    success: function(data, textStatus, jqXHR)
+                    {
+                        if(typeof data.error === 'undefined')
+                        {
+                            // Success so call function to process the form
+//                            submitForm(event, data);
+                        }
+                        else
+                        {
+                            // Handle errors here
+                            console.log('ERRORS: ' + data.error);
+                        }
+                    },
+                    error: function(jqXHR, textStatus, errorThrown)
+                    {
+                        // Handle errors here
+                        console.log('ERRORS: ' + textStatus);
+                        // STOP LOADING SPINNER
+                    }
+                });
+            }
+
+
         });
     </script>
-    -->
+
+
 
     <?php
 }
