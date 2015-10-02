6acc7db47369dc8fab5b35ccb4aed411da0a708b
Kyle B. Johnson <kyle@wpninjas.com>
2015-10-02 15:24:39 -0400

    add file upload validation, renaming, and move to uploads dir.

--- includes/AJAX/Controllers/Uploads.php
+++ includes/AJAX/Controllers/Uploads.php
@@ -15,13 +15,96 @@ class NF_AJAX_Controllers_Uploads extends NF_Abstracts_Controller
 
     }
 
+    /*
+     * PUBLIC METHODS
+     */
+
     public function upload()
     {
         check_ajax_referer( 'ninja_forms_ajax_nonce', 'security' );
 
-        $this->_data['files'] = array_values($_FILES);
+        $this->_data['files'] = $_FILES;
+
+        $this->validate();
 
         $this->_respond();
     }
+
+    /*
+     * PROTECTED METHODS
+     */
+
+    protected function validate()
+    {
+        foreach( $this->_data['files'] as $key => $file ){
+
+            if( $file['error'] ){
+                $this->_errors[] = $this->code_to_message( $file['error'] );
+            } else {
+
+                $upload_dir = wp_upload_dir();
+
+                $new_tmp_name = $this->i_like_clean_slugs_and_i_cannot_lie( $file['name'] );
+
+                move_uploaded_file( $file['tmp_name'], $upload_dir['basedir'] . "/" . $new_tmp_name );
+
+                $this->_data['files'][ $key ]['tmp_name'] = $new_tmp_name;
+            }
+        }
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    protected function i_like_clean_slugs_and_i_cannot_lie( $string )
+    {
+        return 'nftmp-' . strtolower( trim( str_replace(' ', '_', $string ) ) );
+    }
+
+    /*
+     * PRIVATE METHODS
+     */
+
+    /**
+     * Convert $_FILES Error Code to Message
+     *
+     * http://php.net/manual/en/features.file-upload.errors.php
+     *
+     * @param $code
+     * @return string
+     */
+    private function code_to_message($code)
+    {
+        switch ($code) {
+            case UPLOAD_ERR_INI_SIZE:
+                $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message = "The uploaded file was only partially uploaded";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message = "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $message = "Missing a temporary folder";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $message = "Failed to write file to disk";
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $message = "File upload stopped by extension";
+                break;
+
+            default:
+                $message = "Unknown upload error";
+                break;
+        }
+        return $message;
+    }
+
 
 }
