e7f20b98b79f48e9e1d2180b0e1c9dfd1ef59e87
Kyle B. Johnson <kyle@wpninjas.com>
2015-10-07 15:03:45 -0400

    register submission controller

--- ninja-forms.php
+++ ninja-forms.php
@@ -149,6 +149,7 @@ if( defined( 'LOAD_DEPRECATED') AND LOAD_DEPRECATED ) {
                  */
                 self::$instance->controllers[ 'form' ]       = new NF_AJAX_Controllers_Form();
                 self::$instance->controllers[ 'uploads' ]    = new NF_AJAX_Controllers_Uploads();
+                self::$instance->controllers[ 'submission' ] = new NF_AJAX_Controllers_Submission();
 
                 /*
                  * Field Class Registration
