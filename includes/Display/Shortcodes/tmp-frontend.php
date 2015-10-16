fd12ae517ff78092b2a0a389e01a0a1e4c977ecd
Kyle B. Johnson <kyle@wpninjas.com>
2015-10-16 15:08:22 -0400

    remove old testing js

--- includes/Display/Shortcodes/tmp-frontend.php
+++ includes/Display/Shortcodes/tmp-frontend.php
@@ -7,31 +7,3 @@ function nf_tmp_frontend( $atts = array() ) {
 }
 
 add_shortcode( 'nf_tmp_frontend', 'nf_tmp_frontend' );
-
-add_action( 'wp_footer', 'nf_test_ajax', 9001 );
-
-function nf_test_ajax()
-{
-
-	?>
-	<script>
-		jQuery(document).ready(function($) {
-//			var ajaxurl = '<?php //echo admin_url( 'admin-ajax.php' ); ?>//';
-//			var data = {
-//				action: 'nf_process_submission',
-//				nf_form: form_1
-//			}
-
-			console.log( nfForms );
-
-//			jQuery.post(ajaxurl, data, function (response) {
-//
-//				response = JSON.parse( response );
-//
-//				console.log( response );
-//
-//			});
-		});
-	</script>
-	<?php
-}
