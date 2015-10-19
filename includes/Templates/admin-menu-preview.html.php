6aa2a8736fd74f4937d5591b7fd1553b02b26cb4
Kyle B. Johnson <kyle@wpninjas.com>
2015-10-19 11:39:38 -0400

    add page template for temporary preview admin page

--- includes/Templates/admin-menu-preview.html.php
+++ includes/Templates/admin-menu-preview.html.php
@@ -1,3 +1,17 @@
 <div class="wrap">
     Hello, Preview!
 </div>
+
+<script>
+    var data = {
+        action: 'nf_preview_form',
+        security: '<?php echo wp_create_nonce( 'ninja_forms_ajax_nonce' ); ?>',
+        field: { "id" : 1, "settings": { "label": 'This is a field' } }
+    };
+
+    jQuery.post( ajaxurl, data, function(response){
+
+        console.log( JSON.parse( response ) );
+
+    });
+</script>
