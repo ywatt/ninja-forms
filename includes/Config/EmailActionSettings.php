09cc1dd7790764db7fbf7a59bb309278d5a1d02a
Kyle B. Johnson <kyle@wpninjas.com>
2015-10-30 13:58:20 -0400

    stub the email action config file

---
+++ includes/Config/EmailActionSettings.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+return array(
+
+    /*
+     * To
+     */
+
+    'to' => array(
+        'type' => 'text',
+        'label' => __( 'To', 'ninja-forms' ),
+        'placeholder' => __( 'Email address or search for a field', 'ninja-forms' ),
+    ),
+
+);
