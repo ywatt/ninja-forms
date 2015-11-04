27760ccea05c4da749d7e4cff45e31dd449f0f89
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-04 14:36:23 -0500

    add dir and url to cli help

--- includes/WPCLI/NinjaFormsCommand.php
+++ includes/WPCLI/NinjaFormsCommand.php
@@ -15,6 +15,9 @@ class NF_WPCLI_NinjaFormsCommand extends WP_CLI_Command
         WP_CLI::success( 'Welcome to the Ninja Forms WP-CLI Extension!' );
         WP_CLI::line( '' );
         WP_CLI::line( '- Ninja Forms Version: ' . Ninja_Forms::VERSION );
+        WP_CLI::line( '- Ninja Forms Directory: ' . Ninja_Forms::$dir );
+        WP_CLI::line( '- Ninja Forms Public URL: ' . Ninja_Forms::$url );
+        WP_CLI::line( '' );
     }
 
     private function peeking_ninja()
