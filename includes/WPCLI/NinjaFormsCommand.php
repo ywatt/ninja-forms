09df043de6a5c3f7859f9d8c00102dc225fe3799
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-04 14:33:21 -0500

    remove example commands

--- includes/WPCLI/NinjaFormsCommand.php
+++ includes/WPCLI/NinjaFormsCommand.php
@@ -4,42 +4,23 @@ if( ! class_exists( 'WP_CLI_Command' ) ) exit;
 
 class NF_WPCLI_NinjaFormsCommand extends WP_CLI_Command
 {
-    public function __construct()
+    /**
+     * Help Subcommand
+     *
+     * @subcommand help
+     */
+    function help()
     {
-    }
-
-    function __invoke()
-    {
-        WP_CLI::line( $this->peeking_ninja() );
-        WP_CLI::line( 'Welcome to the Ninja Forms WP-CLI Extension!' );
+        $this->peeking_ninja();
+        WP_CLI::success( 'Welcome to the Ninja Forms WP-CLI Extension!' );
         WP_CLI::line( '' );
         WP_CLI::line( '- Ninja Forms Version: ' . Ninja_Forms::VERSION );
     }
 
-    /**
-     * Status Command
-     *
-     * @param $args
-     * @param $assoc_args
-     */
-    function status( $args, $assoc_args )
-    {
-        WP_CLI::line( 'Hello, world!' );
-    }
-
-    function error()
-    {
-        WP_CLI::error( 'It\'s A Trap!' );
-    }
-
-    function success()
-    {
-        WP_CLI::success( 'Fired a WP-CLI Command. Success.' );
-    }
-
     private function peeking_ninja()
     {
-        return file_get_contents( Ninja_Forms::$dir . 'includes/Templates/wpcli-header-art.txt' );
+        $output = file_get_contents( Ninja_Forms::$dir . 'includes/Templates/wpcli-header-art.txt' );
+        WP_CLI::line( $output );
     }
 
 } // END CLASS NF_WPCLI_NinjaFormsCommand
