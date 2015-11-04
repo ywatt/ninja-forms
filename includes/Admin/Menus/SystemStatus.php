ed0f3061eef49eeb64d45184afbc6c8e85b18701
klhall1987@gmail.com <Kenneth@wpninjas.com>
2015-11-04 17:20:09 -0500

    Ported the first half of the system status page from 2.x to 3.0. Some code will need to be refactored

--- includes/Admin/Menus/SystemStatus.php
+++ includes/Admin/Menus/SystemStatus.php
@@ -15,7 +15,86 @@ final class NF_Admin_Menus_SystemStatus extends NF_Abstracts_Submenu
 
     public function display()
     {
-        Ninja_Forms::template( 'admin-menu-system-status.html.php' );
-    }
+        /** @global wpdb $wpdb */
+        global $wpdb;
 
+        $locale = localeconv();
+
+        if ( is_multisite() ) {
+            $multisite = __( 'Yes', 'ninja-forms' );
+        } else {
+            $multisite =  __( 'No', 'ninja-forms' );
+         }
+
+         //TODO: Ask if this check is need
+         //if ( function_exists( 'phpversion' ) ) echo esc_html( phpversion() )
+
+
+         //TODO: Possible refactor
+         foreach( $locale as $key => $val ){
+             if( is_string( $val) ){
+                $data = $key . ': ' . $val . '</br>';
+             }
+         }
+
+         if ( defined('WP_DEBUG') && WP_DEBUG ){
+             $debug = __( 'Yes', 'ninja-forms' );
+         } else {
+            $debug =  __( 'No', 'ninja-forms' );
+         }
+
+         if ( defined( 'WPLANG' ) && WPLANG ) {
+            $lang = WPLANG();
+         } else {
+            $lang = __( 'Default', 'ninja-forms' );
+         }
+
+         //TODO: Ask if this long list of ini_get checks are need?
+
+        //  if( function_exists( 'ini_get' ) ){
+        //     $get_ini = size_format( ini_get('post_max_size') );
+        //  }
+
+        if ( extension_loaded( 'suhosin' ) ) {
+            $suhosin =  __( 'Yes', 'ninja-forms' );
+        } else {
+            $suhosin =  __( 'No', 'ninja-forms' );
+        }
+
+        $default_timezone = date_default_timezone_get();
+        if ( 'UTC' !== $default_timezone ) {
+            echo sprintf( __( 'Default timezone is %s - it should be UTC', 'ninja-forms' ), $default_timezone );
+        } else {
+            echo sprintf( __( 'Default timezone is %s', 'ninja-forms' ), $default_timezone );
+        }
+        $environment = array(
+            __( 'Home URL','ninja-forms' ) => home_url(),
+            __( 'Site URL','ninja-forms' ) => site_url(),
+            __( 'Ninja Forms Version','ninja-forms' ) => esc_html( Ninja_Forms::VERSION ),
+            __( 'WP Version','ninja-forms' ) => get_bloginfo('version'),
+            __( 'WP Multisite Enabled','ninja-forms' ) => $multisite,
+            __( 'Web Server Info','ninja-forms' ) => esc_html( $_SERVER['SERVER_SOFTWARE'] ),
+            __( 'PHP Version','ninja-forms' ) => esc_html( phpversion() ),
+            //TODO: Possibly Refactor with Ninja forms global $_db?
+            __( 'MySQL Version','ninja-forms' ) => $wpdb->db_version(),
+            __( 'PHP Locale','ninja-forms' ) =>  $data,
+            //TODO: Possibly move the ninja_forms_letters_to_numbers function over.
+            __( 'WP Memory Limit','ninja-forms' ) => size_format( WP_MEMORY_LIMIT ),
+            __( 'WP Debug Mode', 'ninja-forms' ) => $debug,
+            __( 'WP Language', 'ninja-forms' ) => $lang,
+            __( 'WP Max Upload Size','ninja-forms' ) => size_format( wp_max_upload_size() ),
+            __('PHP Post Max Size','ninja-forms' ) => size_format( ini_get('post_max_size') ),
+            __('Max Input Nesting Level','ninja-forms' ) => ini_get('max_input_nesting_level'),
+            __('PHP Time Limit','ninja-forms' ) => ini_get('max_execution_time'),
+            __( 'PHP Max Input Vars','ninja-forms' ) => ini_get('max_input_vars'),
+            __( 'SUHOSIN Installed','ninja-forms' ) => $suhosin,
+            __( 'SMTP','ninja-forms' ) => ini_get('SMTP'),
+            __( 'smtp_port','ninja-forms' ) => ini_get('smtp_port'),
+            __( 'Default Timezone','ninja-forms' ) => $default_timezone,
+        );
+
+
+
+        Ninja_Forms::template( 'admin-menu-system-status.html.php', compact( 'environment' ) );
+    }
 } // End Class NF_Admin_SystemStatus
--- includes/Templates/admin-menu-system-status.html.php
+++ includes/Templates/admin-menu-system-status.html.php
@@ -1,5 +1,26 @@
 <div class="wrap">
 
-    Hello, System Status!
+    <div class="nf-box">
+    	<div>
+    		<h4><?php _e( 'Please include this information when requesting support:', 'ninja-forms' ); ?> </h4>
+    		<p class="submit debug-report"><a href="#" class="button-primary"><?php _e( 'Get System Report', 'ninja-forms' ); ?></a></p>
+    	</div>
+    	<div id="debug-report"><textarea readonly="readonly"></textarea></div>
+    </div>
+    <br/>
+    <table class="nf-status-table" cellspacing="0">
+    	<thead>
+    		<tr>
+    			<th colspan="2"><?php _e( 'Environment', 'ninja-forms' ); ?></th>
+    		</tr>
+    	</thead>
+    	<tbody>
+            <?php foreach( $environment as $key => $value ): ?>
+                <tr>
+                    <td><?php echo $key . ':'; ?><td>
+                    <td><?php echo $value; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
 
 </div>
