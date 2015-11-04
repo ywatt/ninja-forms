20899abe142879476a4d1bc2db846689e5d4b5e3
Kevin Stover <kstover@gmail.com>
2015-11-04 15:37:59 -0500

    Changing tmp field IDs on publish.

--- assets/js/builder/controllers/appMenuButtons.js
+++ assets/js/builder/controllers/appMenuButtons.js
@@ -36,7 +36,17 @@ define( [], function() {
 
 			nfRadio.channel( 'app' ).trigger( 'before:sendChanges', data );
 			jQuery.post( ajaxurl, { action: 'nf_save_form', form: data, security: nfAdmin.ajaxNonce }, function( response ) {
-				console.log( response );
+				response = JSON.parse( response );
+				if ( 'undefined' != typeof response.data.new_ids ) {
+					if ( 'undefined' != typeof response.data.new_ids.fields ) {
+						_.each( response.data.new_ids.fields, function( newID, oldID ) {
+							var field = nfRadio.channel( 'data' ).request( 'get:field', oldID );
+							if ( field ) {
+								field.set( 'id', newID );
+							}
+						} );
+					}
+				}
 				nfRadio.channel( 'app' ).trigger( 'response:sendChanges', response );
 			} );
 
