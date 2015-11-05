5c399baad4f513b74a367a547d47ea8d78c4cc43
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-05 16:29:10 -0500

    add config file for settins groups

---
+++ includes/Config/SettingsGroups.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+return array(
+
+    'primary' => array(
+        'id' => 'primary',
+        'label' => '',
+        'display' => TRUE
+    ),
+
+    'restrictions' => array(
+
+    ),
+
+    'advanced' => array(
+
+    )
+
+);
