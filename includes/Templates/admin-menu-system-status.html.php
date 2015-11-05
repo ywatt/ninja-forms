46204ed01ca09b48ca13f4c04c0d4109b903e977
klhall1987@gmail.com <Kenneth@wpninjas.com>
2015-11-05 16:42:41 -0500

    Added new comment to SystemStatus.php and removed excess div then corrected indention in system-status template file.

--- includes/Templates/admin-menu-system-status.html.php
+++ includes/Templates/admin-menu-system-status.html.php
@@ -1,36 +1,34 @@
 <div class="wrap">
     <div class="nf-box">
-    	<div>
     		<h4><?php _e( 'Please include this information when requesting support:', 'ninja-forms' ); ?> </h4>
     		<p class="submit debug-report"><a href="#" class="button-primary"><?php _e( 'Get System Report', 'ninja-forms' ); ?></a></p>
     	</div>
     	<div id="debug-report"><textarea readonly="readonly"></textarea></div>
-    </div>
-    <br/>
-    <table class="nf-status-table" cellspacing="0">
-    	<thead>
-    		<tr>
-    			<th colspan="2"><?php _e( 'Environment', 'ninja-forms' ); ?></th>
-    		</tr>
-    	</thead>
-    	<tbody>
-            <?php foreach( $environment as $key => $value ): ?>
-                <tr>
-                    <td><?php echo $key . ':'; ?><td>
-                    <td><?php echo $value; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-        <thead>
-            <tr>
-                <th colspan="2"><?php _e( 'Plugins', 'ninja-forms' ); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php _e( 'Installed Plugins','ninja-forms' ); ?>:</td>
-                <td><?php echo $site_wide_plugins; ?></td>
-            </tr>
-        </tbody>
-    </table>
 </div>
+<br/>
+<table class="nf-status-table" cellspacing="0">
+	<thead>
+		<tr>
+			<th colspan="2"><?php _e( 'Environment', 'ninja-forms' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+        <?php foreach( $environment as $key => $value ): ?>
+            <tr>
+                <td><?php echo $key . ':'; ?><td>
+                <td><?php echo $value; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+    <thead>
+        <tr>
+            <th colspan="2"><?php _e( 'Plugins', 'ninja-forms' ); ?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php _e( 'Installed Plugins','ninja-forms' ); ?>:</td>
+            <td><?php echo $site_wide_plugins; ?></td>
+        </tr>
+    </tbody>
+</table>
