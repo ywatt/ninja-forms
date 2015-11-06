ae4172e4c46a9a93d9b405da7db0eaef3824d6c2
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-06 17:17:41 -0500

    rename name -> label and update default value label

--- includes/Config/FieldInputSettings.php
+++ includes/Config/FieldInputSettings.php
@@ -9,31 +9,31 @@ return apply_filters( 'ninja-forms-field-input-settings', array(
     array(
         'name'    => 'default',
         'type'    => 'select',
-        'label'   => __( 'Default to User Email (if logged-in)', Ninja_Forms::TEXTDOMAIN ),
+        'label'   => __( 'Default Value', Ninja_Forms::TEXTDOMAIN ),
         'options' => array(
             array(
                 'value' => 'none',
-                'name' => __( 'None', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'None', Ninja_Forms::TEXTDOMAIN )
             ),
             array(
                 'value' => 'post_id',
-                'name' => __( 'Post / Page ID', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'Post / Page ID', Ninja_Forms::TEXTDOMAIN )
             ),
             array(
                 'value' => 'post_title',
-                'name' => __( 'Post / Page Title', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'Post / Page Title', Ninja_Forms::TEXTDOMAIN )
             ),
             array(
                 'value' => 'post_url',
-                'name' => __( 'Post / Page URL', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'Post / Page URL', Ninja_Forms::TEXTDOMAIN )
             ),
             array(
                 'value' => 'query_string',
-                'name' => __( 'Query String Variable', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'Query String Variable', Ninja_Forms::TEXTDOMAIN )
             ),
             array(
                 'value' => 'custom',
-                'name' => __( 'Custom Default Value', Ninja_Forms::TEXTDOMAIN )
+                'label' => __( 'Custom Default Value', Ninja_Forms::TEXTDOMAIN )
             )
         ),
         'width' => 'one-half',
