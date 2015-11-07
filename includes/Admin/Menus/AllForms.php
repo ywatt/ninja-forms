c042ffe0192e4f6271f1892ceac8f86952f37d42
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-07 13:35:03 -0500

    hide extra menu items for Alpha release

--- includes/Admin/Menus/AllForms.php
+++ includes/Admin/Menus/AllForms.php
@@ -12,22 +12,14 @@ final class NF_Admin_Menus_AllForms extends NF_Abstracts_Submenu
     {
         parent::__construct();
 
-        if( ! defined( 'DOING_AJAX' ) ) {
-            add_action('admin_init', array($this, 'admin_init'));
-            add_action( 'admin_init', array( 'NF_Admin_AllFormsTable', 'process_bulk_action' ) );
-        }
+
     }
 
-    public function admin_init()
-    {
-        $this->table = new NF_Admin_AllFormsTable();
-    }
+
 
     public function display()
     {
-        $this->table->prepare_items();
 
-        Ninja_Forms::template( 'admin-menu-all-forms.html.php', array( 'table' => $this->table ) );
     }
 
 }
