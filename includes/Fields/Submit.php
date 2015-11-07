b82be444a51bc813801efb8229d8613b672cbbe0
James Laws <james@wpninjas.com>
2015-11-07 16:06:08 -0500

    Unset certain settings from the submit button.

--- includes/Fields/Submit.php
+++ includes/Fields/Submit.php
@@ -23,6 +23,10 @@ class NF_Fields_Submit extends NF_Fields_Button
 
         $this->_settings = array_merge( $this->_settings, $settings );
 
+        unset( $this->_settings[ 'label_pos' ] );
+        unset( $this->_settings[ 'placeholder' ] );
+        unset( $this->_settings[ 'required' ] );
+
         add_filter( 'nf_sub_hidden_field_types', array( $this, 'hide_field_type' ) );
     }
 
