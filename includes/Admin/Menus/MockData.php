704187f6deed246680feedcf80ba3e3ca0b8c011
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-07 13:41:23 -0500

    add link to all forms table

--- includes/Admin/Menus/MockData.php
+++ includes/Admin/Menus/MockData.php
@@ -15,14 +15,14 @@ final class NF_Admin_Menus_MockData extends NF_Abstracts_Submenu
 
     public function display()
     {
-        echo '<div class="wrap">Hello, migrations!</div>';
-
         $mock_data = new NF_Database_MockData();
 
         $mock_data->form_contact_form_1();
         $mock_data->form_contact_form_2();
         $mock_data->form_long_form();
         $mock_data->form_kitchen_sink();
+
+        echo '<div class="wrap">Migrations and Mock Data complete. <a href="' . admin_url( "admin.php?page=ninja-forms" ) . '">View Forms</a></div>';
     }
 
 } // End Class NF_Admin_Settings
