487d75637c15ec8df52f0f0923a48e48a4623013
klhall1987@gmail.com <Kenneth@wpninjas.com>
2015-11-19 11:53:32 -0500

    Finished adding the rest of the form settings to array.

--- includes/Config/FormSettings.php
+++ includes/Config/FormSettings.php
@@ -2,18 +2,9 @@
 
 return apply_filters( 'ninja-forms-from-settings', array(
 
-    /*
-     * {NAME}
+    /**
+     * DISPLAY
      */
-
-    '{name}' => array(
-        'name' => '{name}',
-        'type' => '{textbox|select|toggle|etc}',
-        'label' => __( '{Name}', 'ninja-forms'),
-        'width' => 'full',
-        'group' => 'primary',
-        'value' => ''
-    ),
 
     /*
      * FORM TITLE
@@ -71,11 +62,86 @@ return apply_filters( 'ninja-forms-from-settings', array(
         'width' => 'full',
         'group' => 'primary',
         'value' => 'true'
+        //TODO: Add following text below the element.
+        //If this box is checked, Ninja Forms will clear the form values after it has been successfully submitted.
     ),
 
     /*
-     *
+     * HIDE SUCCESSFULLY COMPLETED FORMS
      */
 
+    'hide-successfully-completed-form' => array(
+        'name' => 'hide-successfully-completed-form',
+        'type' => 'toggle',
+        'label' => __( 'Hide successfully completed forms?', 'ninja-forms' ),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => 'true'
+        //TODO: Add following text below the element.
+        //If this box is checked, Ninja Forms will hide the form after it has been successfully submitted.
+    ),
+
+    /**
+     * RESTRICTIONS
+     */
+
+    /*
+     * REQUIRE USER TO BE LOGGED IN TO VIEW FORM?
+     */
+
+    'require-user-logged-in-to-view-form' => array(
+        'name' => 'require-user-logged-in-to-view-form',
+        'type' => 'toggle',
+        'label' => __( 'Require user to be logged in to view form?', 'ninja-forms' ),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => 'true'
+    ),
+
+    /*
+     * NOT LOGGED-IN MESSAGE
+     */
+
+    'not-logged-in-message' => array(
+        'name' => 'not-logged-in-message',
+        'type' => '', //TODO: Add WYSIWYG
+        'label' => __( 'Not Logged-In Message', 'ninja-forms' ),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => ''
+        //TODO: Add following text below the WYSIWYG.
+        //Message shown to users if the "logged in" checkbox above is checked and they are not logged-in.
+    ),
+
+    /*
+     * LIMIT SUBMISSIONS
+     */
+
+    'limit-submissions' => array(
+        'name' => 'limit-submissions',
+        'type' => 'textbox',
+        'label' => __( 'Limit Submission', 'ninja-forms' ),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => ''
+        //TODO: Add following text below the element.
+        //Select the number of submissions that this form will accept. Leave empty for no limit.
+    ),
+
+    /*
+     * LIMIT REACHED MESSAGE
+     */
+
+    'limit-reached-message' => array(
+        'name' => 'limit-reached-message',
+        'type' => '',//TODO: Add WYSIWYG
+        'label' => __( 'Limit Reached Message', 'ninja-forms' ),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => ''
+        //TODO: Add following text below the WYSIWYG.
+        //Please enter a message that you want displayed when this form has reached its submission limit and will not
+        //accept new submissions.
+    ),
 
 ));
