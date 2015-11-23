c4de86dcc2cc07cd54e10e95684b952d5f5b0848
Kyle B. Johnson <kyle@wpninjas.com>
2015-11-23 16:09:35 -0500

    nest redierct URL under data -> actions -> redirect

--- includes/Actions/Redirect.php
+++ includes/Actions/Redirect.php
@@ -50,7 +50,7 @@ final class NF_Actions_Redirect extends NF_Abstracts_Action
 
     public function process( $action_settings, $form_id, $data )
     {
-        $data['redirect'] = $action_settings[ 'url' ];
+        $data[ 'actions' ][ 'redirect' ] = $action_settings[ 'url' ];
 
         return $data;
     }
