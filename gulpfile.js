a9eb90b9531709b579482bcaa182b83a35900db6
Kevin Stover <kstover@gmail.com>
2015-11-24 17:25:59 -0500

    Using Gulp to compile and minify js and scss files.

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,75 @@
 var gulp   = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('js', function () {
-    gulp.src('assets/js/dev/nf-upgrade-handler.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('assets/js/min/')); //the destination folder
+    gulp.src('assets/js/builder/main.js')
+    .pipe(sourcemaps.init())
+    .pipe(requirejsOptimize(function(file) {
+        return {
+            name: '../lib/almond',
+            optimize: 'uglify2',
+            wrap: true,
+            baseUrl: 'assets/js/builder/',
+            include: ['main'],
+            preserveLicenseComments: false
+        };
+    }))
+    .pipe(rename('builder.js'))
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('assets/js/min/'));
 
-    gulp.src('js/dev/ninja-forms-admin.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js/min/')); //the destination folder
+    gulp.src('assets/js/front-end/main.js')
+    .pipe(sourcemaps.init())
+    .pipe(requirejsOptimize(function(file) {
+        return {
+            name: '../lib/almond',
+            optimize: 'uglify2',
+            wrap: true,
+            baseUrl: 'assets/js/front-end/',
+            include: ['main'],
+            preserveLicenseComments: false
+        };
+    }))
+    .pipe(rename('front-end.js'))
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('assets/js/min/'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('assets/scss/admin/builder.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('assets/css'));   
+
+    gulp.src('assets/scss/front-end/display-structure.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('assets/css'));
+
+    gulp.src('assets/scss/front-end/display-opinions.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('assets/css'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/dev/ninja-forms-admin.js', ['js']);
-    gulp.watch('assets/js/dev/nf-upgrade-handler.js', ['js']);
+    gulp.watch('assets/js/builder/**/*.js', ['js']);
+    gulp.watch('assets/js/front-end/**/*.js', ['js']);
+    gulp.watch('assets/scss/**/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'sass', 'watch']);
 
 function swallowError (error) {
     //If you want details of the error in the console
