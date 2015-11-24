82fdfd5281a953f844d0afd70c9650ebaa9074d4
Kevin Stover <kstover@gmail.com>
2015-11-24 09:43:18 -0500

    Updating version number in the main plugin file.

--- ninja-forms.php
+++ ninja-forms.php
@@ -3,7 +3,7 @@
 Plugin Name: Ninja Forms - Three Alpha
 Plugin URI: http://ninjaforms.com/
 Description: Ninja Forms is a webform builder with unparalleled ease of use and features.
-Version: 3.0-Alpha-1-Troughton
+Version: 3.0-Alpha-3-Pertwee
 Author: The WP Ninjas
 Author URI: http://ninjaforms.com
 Text Domain: ninja-forms
