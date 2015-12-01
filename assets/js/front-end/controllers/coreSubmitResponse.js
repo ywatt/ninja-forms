33c830c5916adbb3bbb6e75f2b0e929932e8fa1c
Kevin Stover <kstover@gmail.com>
2015-11-30 21:53:00 -0500

    Display Form Title, Clear, Hide successful form should now work after submission.

---
+++ assets/js/front-end/controllers/coreSubmitResponse.js
@@ -0,0 +1,21 @@
+define([], function() {
+	var controller = Marionette.Object.extend( {
+		initialize: function() {
+			this.listenTo( nfRadio.channel( 'submit' ), 'submit:response', this.actionSubmit );
+		},
+
+		actionSubmit: function( response ) {
+			if ( 1 == response.data.settings.clear_successfully_created_form ) {
+				
+			}
+
+			if ( 1 == response.data.settings.hide_successfully_completed_form ) {
+				jQuery( '.nf-fields' ).hide();
+				jQuery( '.nf-form-title' ).hide();
+			}
+		}
+
+	});
+
+	return controller;
+} );
