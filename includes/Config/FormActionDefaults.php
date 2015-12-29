f9ddd8a5e09e84fc655dc6503c02fd1a7a1defa1
Kyle B. Johnson <kyle@wpninjas.com>
2015-12-29 15:26:04 -0500

    add order properties for default actions

--- includes/Config/FormActionDefaults.php
+++ includes/Config/FormActionDefaults.php
@@ -3,28 +3,31 @@
 return apply_filters( 'ninja-forms-from-display-settings', array(
 
     array(
-        'id' => 'tmp-1',
-        'label' => 'Success Message',
-        'type' => 'successmessage',
+        'id'      => 'tmp-1',
+        'label'   => 'Success Message',
+        'type'    => 'successmessage',
         'message' => 'This is a default success message',
-        'active' => TRUE,
+        'order'   => 1,
+        'active'  => TRUE,
     ),
 
     array(
-        'id' => 'tmp-2',
-        'label' => 'Admin Email',
-        'type' => 'email',
-        'to' => array( 'myformbuildingbringsallthedeveloperstotheyard@wpninjas.com' ),
+        'id'      => 'tmp-2',
+        'label'   => 'Admin Email',
+        'type'    => 'email',
+        'to'      => array( 'myformbuildingbringsallthedeveloperstotheyard@wpninjas.com' ),
         'subject' => 'This is an email action.',
         'message' => 'Hello, Ninja Forms!',
-        'active' => TRUE,
+        'order'   => 2,
+        'active'  => TRUE,
     ),
 
     array(
-        'id' => 'tmp-3',
+        'id'    => 'tmp-3',
         'label' => 'Save Submission',
-        'type' => 'save',
-        'active' => TRUE,
+        'type'  => 'save',
+        'order' => 3,
+        'active'=> TRUE,
     ),
 
 ));
