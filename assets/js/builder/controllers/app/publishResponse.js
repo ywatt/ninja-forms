c990e53c7f18c3692da071b86901a1f4b9bcedcd
Kyle B. Johnson <kyle@wpninjas.com>
2015-12-29 13:30:38 -0500

    update URL with new form id

--- assets/js/builder/controllers/app/publishResponse.js
+++ assets/js/builder/controllers/app/publishResponse.js
@@ -41,6 +41,13 @@ define( [], function() {
 						}
 					} );
 				}
+
+				// If we have any new actions, update their models with the new id.
+				if ( 'undefined' != typeof response.data.new_ids.forms ) {
+					_.each( response.data.new_ids.forms, function( newID, oldID ) {
+						history.replaceState( '', '', 'admin.php?page=ninja-forms&form_id=' + newID );
+					} );
+				}
 			}
 
 			nfRadio.channel( 'app' ).request( 'update:setting', 'loading', false );
