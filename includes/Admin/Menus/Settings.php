bc82ef6b352b3bed275489cc2acf266ec4a34897
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-04 12:09:32 -0500

    add empty defalut values for development

--- includes/Admin/Menus/Settings.php
+++ includes/Admin/Menus/Settings.php
@@ -17,10 +17,13 @@ final class NF_Admin_Menus_Settings extends NF_Abstracts_Submenu
     public function display()
     {
         $grouped_settings[ 'general' ]   = Ninja_Forms()->config( 'PluginSettingsGeneral' );
-
         $grouped_settings[ 'advanced' ]  = Ninja_Forms()->config( 'PluginSettingsAdvanced' );
-
         $grouped_settings[ 'recaptcha' ] = Ninja_Forms()->config( 'PluginSettingsReCaptcha' );
+
+        foreach( $grouped_settings as $group => $settings ){
+            foreach( $settings as $key => $setting ){}
+            $grouped_settings[ $group ][ $key ][ 'value' ] = '';
+        }
 
         $grouped_settings[ 'general' ][ 'version' ][ 'value' ] = Ninja_Forms::VERSION;
 
