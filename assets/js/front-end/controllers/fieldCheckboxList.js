250fd4c2836f3ec6b58e1beec7bc476e09bed495
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-04 22:33:56 -0500

    add controller for checkbox list field options

---
+++ assets/js/front-end/controllers/fieldCheckboxList.js
@@ -0,0 +1,76 @@
+define([], function() {
+    var controller = Marionette.Object.extend( {
+        initialize: function() {
+            this.listenTo( nfRadio.channel( 'listcheckbox' ), 'change:modelValue', this.changeModelValue );
+            this.listenTo( nfRadio.channel( 'listcheckbox' ), 'init:model', this.register );
+        },
+
+        register: function( model ) {
+            model.set( 'renderOptions', this.renderOptions );
+            model.set( 'renderOtherText', this.renderOtherText );
+        },
+
+        changeModelValue: function( model ) {
+            if ( 1 == model.get( 'show_other' ) ) {
+                model.set( 'reRender', true );
+            }
+        },
+
+        renderOptions: function() {
+            var that = this;
+            var html = '';
+            if ( '' == this.value ) {
+                var valueFound = true;
+            } else {
+                var valueFound = false;
+            }
+
+            _.each( this.options, function( option ) {
+                if ( option.value == that.value ) {
+                    valueFound = true;
+                }
+
+                option.fieldID = that.id;
+                option.classes = that.classes;
+                option.currentValue = that.value;
+
+                console.log( option.selected );
+
+                html += _.template( jQuery( '#nf-tmpl-field-listcheckbox-option' ).html(), option );
+            } );
+
+            if ( 1 == this.show_other ) {
+                if ( 'nf-other' == this.value ) {
+                    valueFound = false;
+                }
+                var data = {
+                    fieldID: this.id,
+                    classes: this.classes,
+                    currentValue: this.value,
+                    renderOtherText: this.renderOtherText,
+                    valueFound: valueFound
+                };
+                html += _.template( jQuery( '#nf-tmpl-field-listcheckbox-other' ).html(), data );
+
+            }
+
+            return html;
+        },
+
+        renderOtherText: function() {
+            if ( 'nf-other' == this.currentValue || ! this.valueFound ) {
+                if ( 'nf-other' == this.currentValue ) {
+                    this.currentValue = '';
+                }
+                var data = {
+                    fieldID: this.fieldID,
+                    classes: this.classes,
+                    currentValue: this.currentValue
+                };
+                return _.template( jQuery( '#nf-tmpl-field-listcheckbox-other-text' ).html(), data );
+            }
+        }
+    });
+
+    return controller;
+} );
--- includes/Fields/ListCheckbox.php
+++ includes/Fields/ListCheckbox.php
@@ -21,4 +21,21 @@ class NF_Fields_ListCheckbox extends NF_Abstracts_List
 
         $this->_nicename = __( 'Checkbox List', 'ninja-forms' );
     }
+
+    public function admin_form_element( $id, $value )
+    {
+        echo "<pre>";
+        var_dump($value);
+        echo "</pre>";
+
+//        $field = Ninja_Forms()->form()->get_field( $id );
+//
+//        $options = '<option>--</option>';
+//        foreach( $field->get_setting( 'options' ) as $option ){
+//            $selected = ( $value == $option[ 'value' ] ) ? "selected" : '';
+//            $options .= "<option value='{$option[ 'value' ]}' $selected>{$option[ 'label' ]}</option>";
+//        }
+//
+//        return "<select class='widefat' name='fields[$id]' id=''>$options</select>";
+    }
 }
