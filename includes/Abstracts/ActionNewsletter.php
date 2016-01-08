3fd45c21fc73565b93790348d99979cbe5659727
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-07 22:25:51 -0500

    stub an abstract newsletter action.

---
+++ includes/Abstracts/ActionNewsletter.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Class NF_Abstracts_ActionNewsletter
+ */
+final class NF_Abstracts_ActionNewsletter extends NF_Abstracts_Action
+{
+    /**
+     * @var array
+     */
+    protected $_tags = array( 'newsletter' );
+
+    /**
+     * @var string
+     */
+    protected $_timing = 'normal';
+
+    /**
+     * @var int
+     */
+    protected $_priority = '10';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /*
+    * PUBLIC METHODS
+    */
+
+    public function save()
+    {
+
+    }
+
+    public function process( $action_settings, $form_id, $data )
+    {
+
+    }
+}
