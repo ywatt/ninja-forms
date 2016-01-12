38474545ade3fa31a55cc32322446b7707600869
Kevin Stover <kstover@gmail.com>
2016-01-12 14:33:48 -0500

    More work on the front-end display of calculations.

--- assets/js/front-end/models/calcModel.js
+++ assets/js/front-end/models/calcModel.js
@@ -3,12 +3,38 @@ define( [], function() {
 		initialize: function() {
 			// Set our form id
 			this.set( 'formID', this.collection.options.formModel.get( 'id' ) );
-
+			this.fields = {};
+			var that = this;
 			var eq = this.get( 'eq' );
+			var eqValues = eq;
 			// Check to see if we have any merge tags in our equation.
-			console.log( eq.split( '{field:' ).split( '}' ) );
+			var fields = eq.match( new RegExp( /{field:(.*?)}/g ) );
+			fields = fields.map( function( field ) {
+				var key = field;
+				key = key.replace( '}', '' ).replace( '{field:', '' ); 
+				fieldModel = nfRadio.channel( 'form-' + that.get( 'formID' ) ).request( 'get:fieldByKey', key );
+				fieldModel.on( 'change:value', that.changeField, that );
+				var value = ( jQuery.isNumeric( fieldModel.get( 'value' ) ) ) ? fieldModel.get( 'value' ): 0;
+				that.fields[ key ] = value;
+				eqValues = eqValues.replace( field, value );
+			} );
+
+			this.set( 'value', math.eval( eqValues ) );
+			console.log( this.get( 'value' ) );
 			// Send a message out on our form channel asking for the fieldModel that corresponds to any merge tags found in this equation.
 			
+		},
+
+		changeField: function( fieldModel ) {
+			var key = fieldModel.get( 'key' );
+			var value = ( jQuery.isNumeric( fieldModel.get( 'value' ) ) ) ? fieldModel.get( 'value' ): 0;
+			this.fields[ key ] = value;
+			var eqValues = this.get( 'eq' );
+			_.each( this.fields, function( value, key ) {
+				eqValues = eqValues.replace( '{field:' + key + '}', value );
+			} );
+			this.set( 'value', math.eval( eqValues ) );
+			console.log( this.get( 'value' ) );
 		}
 	} );
 
--- assets/js/front-end/models/formModel.js
+++ assets/js/front-end/models/formModel.js
@@ -5,6 +5,14 @@ define( [], function() {
 			afterForm: '',
 			beforeFields: '',
 			afterFields: ''
+		},
+
+		initialize: function() {
+			nfRadio.channel( 'form-' + this.get( 'id' ) ).reply( 'get:fieldByKey', this.getFieldByKey, this );
+		},
+
+		getFieldByKey: function( key ) {
+			return this.get( 'fields' ).findWhere( { key: key } );
 		}
 	} );
 
