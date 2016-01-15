111566f9488ad7e381ebc95772315eccb08f421e
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-15 09:45:55 -0500

    add support for calculations as default value for hidden field.

--- includes/Fields/Hidden.php
+++ includes/Fields/Hidden.php
@@ -25,8 +25,11 @@ class NF_Fields_Hidden extends NF_Abstracts_Input
     {
         parent::__construct();
 
+        $use_merge_tags = array( 'include' => array( 'calculations' ) );
+
         $this->_settings[ 'label' ][ 'width' ] = 'full';
         $this->_settings[ 'default' ][ 'group' ] = 'primary';
+        $this->_settings[ 'default' ][ 'user_merge_tags' ] = $use_merge_tags;
 
         $this->_nicename = __( 'Hidden', 'ninja-forms' );
     }
