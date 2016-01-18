f24d13e48420bd836a23cdc743e85bb81a8937ec
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-18 15:17:46 -0500

    Update preview class to limit query to 1 post. Closes  #1085 .

--- includes/Display/Preview.php
+++ includes/Display/Preview.php
@@ -13,10 +13,17 @@ final class NF_Display_Preview
 
         $this->_form_id = $_GET['nf_preview_form'];
 
+        add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
+
         add_filter('the_title', array( $this, 'the_title' ) );
-        add_filter('the_content', array( $this, 'the_content' ) );
+        add_filter('the_content', array( $this, 'the_content' ), 9001 );
         add_filter('get_the_excerpt', array( $this, 'the_content' ) );
         add_filter('template_include', array( $this, 'template_include' ) );
+    }
+
+    public function pre_get_posts( $query )
+    {
+        $query->set( 'posts_per_page', 1 );
     }
 
     /**
@@ -34,7 +41,7 @@ final class NF_Display_Preview
      */
     function the_content()
     {
-        return "[nf_tmp_preview id='{$this->_form_id}']";
+        return do_shortcode( "[nf_tmp_preview id='{$this->_form_id}']" );
     }
 
     /**
