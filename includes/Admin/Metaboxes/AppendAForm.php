59a685b77053e4521f28ae0d08f15a61d1bf984d
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-18 13:06:23 -0500

    fix post metabox saving. #1081 .

--- includes/Admin/Metaboxes/AppendAForm.php
+++ includes/Admin/Metaboxes/AppendAForm.php
@@ -11,15 +11,37 @@ final class NF_Admin_Metaboxes_AppendAForm extends NF_Abstracts_Metabox
         $this->_title = __( 'Append a Ninja Forms', 'ninja-forms' );
     }
 
-    protected function save_post( $post_id )
+    public function save_post( $post_id )
     {
-        // TODO: Save
+        if (
+            defined('DOING_AUTOSAVE') && DOING_AUTOSAVE
+            || ! isset( $_POST['nf_append_form'] )
+            || ! wp_verify_nonce( $_POST['nf_append_form'], 'ninja_forms_append_form' )
+            || ( 'page' == $_POST['post_type'] && !current_user_can( 'edit_page', $post_id ) )
+            || ( 'post' == $_POST['post_type'] && !current_user_can( 'edit_post', $post_id ) )
+        ) return $post_id;
+
+        $post_id = absint( $post_id );
+
+        $form_id = absint( $_POST['ninja_form_select'] );
+
+        if ( empty ( $form_id ) ) {
+            delete_post_meta( $post_id, 'ninja_forms_form' );
+        } else {
+            update_post_meta( $post_id, 'ninja_forms_form', $form_id );
+        }
     }
 
     public function render_metabox( $post, $metabox )
     {
+        wp_nonce_field( 'ninja_forms_append_form', 'nf_append_form' );
+
         $forms = Ninja_Forms()->form()->get_forms();
 
-        Ninja_Forms()->template( 'admin-metabox-append-a-form.html.php', compact( 'forms' ) );
+        $form_id = get_post_meta( $post->ID, 'ninja_forms_form', true );
+
+        $none_text = '-- ' . __( 'None', 'ninja-forms' );
+
+        Ninja_Forms()->template( 'admin-metabox-append-a-form.html.php', compact( 'forms', 'form_id', 'none_text' ) );
     }
 }
--- includes/Templates/admin-metabox-append-a-form.html.php
+++ includes/Templates/admin-metabox-append-a-form.html.php
@@ -1,9 +1,12 @@
-<select>
-<?php foreach( $forms as $form ): ?>
+<select name='ninja_form_select'>
+    <option value="0"><?php echo $none_text;?></option>
+    <?php foreach( $forms as $form ): ?>
 
-    <option value="<?php echo $form->get_id(); ?>">
-        <?php echo $form->get_setting( 'title' ); ?>
-    </option>
+        <?php $id = $form->get_id(); ?>
 
-<?php endforeach; ?>
+        <option value="<?php echo $id; ?>"<?php selected( $id, $form_id );?>>
+            <?php echo $form->get_setting( 'title' ); ?>
+        </option>
+
+    <?php endforeach; ?>
 </select>
