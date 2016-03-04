5b6653453e7786bd93d41269f5a8d8abe0b8f493
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-04 15:18:17 -0500

    add undefined check before checking for options.

--- assets/js/builder/models/app/settingModel.js
+++ assets/js/builder/models/app/settingModel.js
@@ -31,6 +31,9 @@ define( [], function() {
 			 * If we have an objectType set on our collection, then we're creating a model for the generic settings collection.
 			 * If we're using merge tags in this setting
 			 */
+
+			if( 'undefined' == typeof this.collection ) return;
+
 			if ( this.get( 'use_merge_tags' ) && 'undefined' != typeof this.collection.options.objectType ) {
 				this.listenTo( nfRadio.channel( 'app' ), 'update:fieldKey', this.updateKey );
 			}
