78197bf198a8d3546f22d3b5707fcc948c799baf
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-04 13:54:24 -0500

    add action type check.

--- includes/AJAX/Controllers/Submission.php
+++ includes/AJAX/Controllers/Submission.php
@@ -174,6 +174,8 @@ class NF_AJAX_Controllers_Submission extends NF_Abstracts_Controller
 
             $action_settings[ 'id' ] = $action->get_id();
 
+            if( ! isset( Ninja_Forms()->actions[ $type ] ) ) continue;
+
             $data = Ninja_Forms()->actions[ $type ]->process( $action_settings, $this->_form_id, $this->_data );
 
             $this->_data = ( $data ) ? $data : $this->_data;
