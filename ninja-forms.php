22d28fc2e9e453460f3093950663f72fc331b301
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-04 11:14:39 -0500

    remove freemius for second take.

--- ninja-forms.php
+++ ninja-forms.php
@@ -12,6 +12,8 @@ Domain Path: /lang/
 Copyright 2015 WP Ninjas.
 */
 
+new NF_VersionSwitcher();
+
 if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! isset( $_POST[ 'nf2to3' ] ) ) {
 
     include 'deprecated/ninja-forms.php';
@@ -549,10 +551,6 @@ if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! isset( $_POST[ 'nf2t
 
 final class NF_VersionSwitcher
 {
-    private $freemius;
-
-    private $public_key = 'pk_a3fdd225c22bcc5b31224fc59ff3a';
-
     public function __construct()
     {
         $this->listener();
@@ -565,11 +563,9 @@ final class NF_VersionSwitcher
 
             switch( $_GET[ 'nf-switcher' ] ){
                 case 'upgrade':
-                    add_action( 'plugins_loaded', array( $this, 'upgrade' ) );
                     update_option( 'ninja_forms_load_deprecated', FALSE );
                     break;
                 case 'rollback':
-                    add_action( 'plugins_loaded', array( $this, 'downgrade' ) );
                     update_option( 'ninja_forms_load_deprecated', TRUE );
                     break;
             }
@@ -599,76 +595,4 @@ final class NF_VersionSwitcher
         $wp_admin_bar->add_node($args);
     }
 
-    public function freemius()
-    {
-        if( ! $this->freemius ){
-
-            require_once dirname( __FILE__ ) . '/includes/Libraries/Freemius/start.php';
-
-            $this->freemius = fs_dynamic_init( array(
-                'id'                => '212',
-                'slug'              => 'ninja-forms',
-                'public_key'        => $this->public_key,
-                'is_live'           => false,
-                'is_premium'        => false,
-                'has_addons'        => false,
-                'has_paid_plans'    => false,
-                'menu'              => array(
-                    'slug'       => 'ninja-forms',
-                    'account'    => false,
-                    'support'    => false,
-                ),
-            ) );
-        }
-
-        return $this->freemius;
-    }
-
-    public function upgrade()
-    {
-        update_option( 'ninja_forms_version', '3.0' );
-        // Turn Freemius on.
-        update_option( 'ninja_forms_freemius', 1 );
-
-        $this->override_plugin_version();
-
-        if ( ! $this->freemius()->is_registered() && $this->freemius()->has_api_connectivity() ) {
-            if ( $this->freemius()->opt_in() ) {
-                // Successful opt-in into Freemius.
-            }
-        } else if ( $this->freemius()->is_registered() ) {
-            // Send immediate re-upgrade event.
-            $this->freemius()->_run_sync_install();
-        }
-    }
-
-
-    public function downgrade()
-    {
-        update_option( 'ninja_forms_version', '2.9' );
-
-        if ( $this->freemius()->is_registered() ) {
-            // Send immediate downgrade event.
-            $this->freemius()->_run_sync_install();
-        }
-    }
-
-    public function opt_out()
-    {
-        $this->freemius()->delete_account_event();
-
-        // Turn Freemius off.
-        update_option( 'ninja_forms_freemius', 0 );
-    }
-
-    public function override_plugin_version()
-    {
-        // Init Freemius and override plugin's version.
-        if ( ! has_filter( 'fs_plugin_version_ninja-forms' ) ) {
-            add_filter( 'fs_plugin_version_ninja-forms', 'nf_plugin_version' );
-        }
-    }
-
 }
-new NF_VersionSwitcher();
-
