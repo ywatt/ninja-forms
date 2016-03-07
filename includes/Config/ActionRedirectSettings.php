9cb0677760b8f21a6cd3b75864a5bc86fcd6a2e3
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-07 09:21:02 -0500

    update redirect url setting name.

--- includes/Config/ActionRedirectSettings.php
+++ includes/Config/ActionRedirectSettings.php
@@ -3,11 +3,11 @@
 return array(
 
     /*
-    * URL
+    * Redirect URL
     */
 
-    'url' => array(
-        'name' => 'url',
+    'redirect_url' => array(
+        'name' => 'redirect_url',
         'type' => 'textbox',
         'group' => 'primary',
         'label' => __( 'URL', 'ninja-forms' ),
