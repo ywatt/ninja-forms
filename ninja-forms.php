0cb21b22874c0cc85943acb0d02622af6b4d2d02
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-17 11:40:57 -0400

    activate Freemius on 2.9.x activation.

--- ninja-forms.php
+++ ninja-forms.php
@@ -25,7 +25,7 @@ if( get_option( 'ninja_forms_load_deprecated', FALSE )  && ! isset( $_POST[ 'nf2
     register_activation_hook( __FILE__, 'ninja_forms_activation_deprecated' );
     function ninja_forms_activation_deprecated( $network_wide ){
         include_once 'deprecated/includes/activation.php';
-        // update_option( 'ninja_forms_freemius', 1 );
+        update_option( 'ninja_forms_freemius', 1 );
         ninja_forms_activation( $network_wide );
     }
 
