cbed903718824902ad08be541f9a72417aa20a60
Kevin Stover <kstover@gmail.com>
2016-03-17 09:44:18 -0400

    Removing un-necessary files.

--- assets/js/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-r.js -o baseUrl=builder/ out=min/builder.js optimize=uglify2 name=../lib/almond include=main wrap=true
-r.js -o baseUrl=front-end/ out=min/front-end.js optimize=uglify2 name=../lib/almond include=main wrap=true
