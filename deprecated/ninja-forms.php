f316dd25b0d3f514a2948736ae0375056139d5cb
Kyle B. Johnson <kyle@wpninjas.com>
2016-03-17 10:57:49 -0400

    require upgrade class for uninstall.

--- deprecated/ninja-forms.php
+++ deprecated/ninja-forms.php
@@ -944,6 +944,7 @@ function ninja_forms_uninstall(){
 
 
         // Remove upgrade last step options
+        require_once 'includes/admin/upgrades/class-upgrade-handler.php';
         $upgrades = NF_UpgradeHandler()->upgrades;
 
         if ($upgrades AND is_array($upgrades)) {
