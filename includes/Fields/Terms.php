e7845fe355d0cb7c7f0e137aa83555c541afced9
Kyle B. Johnson <kyle@wpninjas.com>
2016-04-12 15:44:10 -0400

    add term option selection.

--- includes/Fields/Terms.php
+++ includes/Fields/Terms.php
@@ -8,7 +8,7 @@ class NF_Fields_Terms extends NF_Fields_ListCheckbox
     protected $_name = 'terms';
     protected $_type = 'terms';
 
-    protected $_nicename = 'Terms';
+    protected $_nicename = 'Terms List';
 
     protected $_section = '';
 
@@ -22,18 +22,41 @@ class NF_Fields_Terms extends NF_Fields_ListCheckbox
     {
         parent::__construct();
 
-        $this->_nicename = __( 'Terms', 'ninja-forms' );
+        $this->_nicename = __( 'Terms List', 'ninja-forms' );
 
         add_filter( 'ninja_forms_localize_field_' . $this->_type, array( $this, 'add_term_options' ) );
         add_filter( 'ninja_forms_localize_field_' . $this->_type . '_preview', array( $this, 'add_term_options' ) );
 
+        $term_settings = array();
         $taxonomies = get_taxonomies( '', 'objects' );
         foreach( $taxonomies as $name => $taxonomy ){
             $this->_settings[ 'taxonomy' ][ 'options' ][] = array(
                 'label' => $taxonomy->labels->name,
                 'value' => $name
             );
+
+            $terms = get_terms( $name );
+            foreach( $terms as $term ){
+                $term_settings[] =  array(
+                    'name' => 'taxonomy_term_' . $term->term_id,
+                    'type' => 'toggle',
+                    'label' => $term->name,
+                    'width' => 'one-third',
+                    'deps' => array(
+                        'taxonomy' => $name
+                    )
+                );
+            }
         }
+
+        $this->_settings[ 'taxonomy_terms' ] = array(
+            'name' => 'taxonomy_terms',
+            'type' => 'fieldset',
+            'label' => __( 'Available Terms' ),
+            'width' => 'full',
+            'group' => 'primary',
+            'settings' => $term_settings,
+        );
 
         $this->_settings[ 'options' ][ 'group' ] = '';
     }
@@ -44,15 +67,18 @@ class NF_Fields_Terms extends NF_Fields_ListCheckbox
 
         $terms = get_terms( $field[ 'settings' ][ 'taxonomy' ] );
 
+        $field['settings']['options'] = array();
         foreach( $terms as $term ) {
-            $field['settings']['options'] = array(
-                array(
-                    'label' => $term->name,
-                    'value' => $term->term_id,
-                    'calc' => '',
-                    'selected' => 0,
-                    'order' => 0
-                )
+
+            if( ! isset( $field[ 'settings' ][ 'taxonomy_term_' . $term->term_id ] ) ) continue;
+            if( ! $field[ 'settings' ][ 'taxonomy_term_' . $term->term_id ] ) continue;
+
+            $field['settings']['options'][] = array(
+                'label' => $term->name,
+                'value' => $term->term_id,
+                'calc' => '',
+                'selected' => 0,
+                'order' => 0
             );
         }
 
