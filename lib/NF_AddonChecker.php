9f9802d93f3bcafd544eaa4639e24f4151a8e406
Kyle B. Johnson <kyle@wpninjas.com>
2016-04-28 23:27:36 -0400

    add function check.

--- lib/NF_AddonChecker.php
+++ lib/NF_AddonChecker.php
@@ -10,6 +10,10 @@ final class NF_AddonChecker
 
     public function check_plugins()
     {
+        if ( ! function_exists( 'get_plugins' ) ) {
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
+        
         $plugins = get_plugins();
 
         foreach( $plugins as $plugin => $data ){
