913a31b3d1486e734b80f815846e7c07711455da
Kevin Stover <kstover@gmail.com>
2016-05-20 11:39:02 -0400

    Added a class to the field container for description position.

--- assets/js/front-end/views/fieldLayout.js
+++ assets/js/front-end/views/fieldLayout.js
@@ -20,7 +20,11 @@ define( ['views/fieldItem', 'views/beforeField', 'views/afterField'], function( 
             return {
                 renderContainerClass: function() {
                     var containerClass = ' label-' + this.label_pos + ' ';
-                    // if we have a container_class field setting, add that to our wrap.
+                    // If we have a description position, add that to our container.
+                    if ( 'undefined' != typeof this.desc_pos ) {
+                        containerClass += 'desc-' + this.desc_pos + ' ';
+                    }
+                    // if we have a container_class field setting, add that to our container.
                     if ( 'undefined' != typeof this.container_class && 0 < jQuery.trim( this.container_class ).length ) {
                         containerClass += this.container_class + ' ';
                     }
