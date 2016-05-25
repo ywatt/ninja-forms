f04232df047ee3913d0c025483a6f6f439a28114
Kyle B. Johnson <kyle@wpninjas.com>
2016-05-25 17:13:42 -0400

    Separate form/field errors. Closes #1369 .

--- assets/js/front-end/controllers/submit.js
+++ assets/js/front-end/controllers/submit.js
@@ -79,7 +79,7 @@ define([], function() {
 			    cache: false,
 			   	success: function( data, textStatus, jqXHR ) {
 			   		var response = jQuery.parseJSON( data );
-			        nfRadio.channel( 'forms' ).trigger( 'submit:response', response, textStatus, jqXHR );
+			        nfRadio.channel( 'forms' ).trigger( 'submit:response', response, textStatus, jqXHR, formModel.get( 'id' ) );
 			    	nfRadio.channel( 'form-' + formModel.get( 'id' ) ).trigger( 'submit:response', response, textStatus, jqXHR )
 			    },
 			    error: function( jqXHR, textStatus, errorThrown ) {
