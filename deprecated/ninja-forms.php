966c770be16a4bc832745eed174362471c42232b
Kevin Stover <kstover@gmail.com>
2016-06-21 15:02:41 -0400

    Preparing for the release of version 2.9.50.

--- deprecated/ninja-forms.php
+++ deprecated/ninja-forms.php
@@ -265,7 +265,7 @@ class Ninja_Forms {
 
         // Plugin version
         if ( ! defined( 'NF_PLUGIN_VERSION' ) )
-            define( 'NF_PLUGIN_VERSION', '2.9.49' );
+            define( 'NF_PLUGIN_VERSION', '2.9.50' );
 
         // Plugin Folder Path
         if ( ! defined( 'NF_PLUGIN_DIR' ) )
