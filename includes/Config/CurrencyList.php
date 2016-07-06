ec74574cc9743398f603828091a1732b65adfc92
Kyle B. Johnson <kyle@wpninjas.com>
2016-07-06 12:01:56 -0400

    extract currency list from Stripe to core config file. #1476.

---
+++ includes/Config/CurrencyList.php
@@ -0,0 +1,110 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+return apply_filters( 'ninja_forms_config_currency_list', array(
+
+    'AUD' => array(
+        'name' => __( 'Australian Dollars', 'ninja-forms' ),
+        'value' => 'AUD'
+    ),
+
+    'CAD' => array(
+        'name' => __( 'Canadian Dollars', 'ninja-forms' ),
+        'value' => 'CAD'
+    ),
+
+    'CZK' => array(
+        'name' => __( 'Czech Koruna', 'ninja-forms' ),
+        'value' => 'CZK'
+    ),
+
+    'DKK' => array(
+        'name' => __( 'Danish Krone', 'ninja-forms' ),
+        'value' => 'DKK'
+    ),
+
+    'EUR' => array(
+        'name' => __( 'Euros', 'ninja-forms' ),
+        'value' => 'EUR'
+    ),
+
+    'HKD' => array(
+        'name' => __( 'Hong Kong Dollars', 'ninja-forms' ),
+        'value' => 'HKD'
+    ),
+
+    'HUF' => array(
+        'name' => __( 'Hungarian Forints', 'ninja-forms' ),
+        'value' => 'HUF'
+    ),
+
+    'ILS' => array(
+        'name' => __( 'Israeli New Sheqels', 'ninja-forms' ),
+        'value' => 'ILS'
+    ),
+
+    'JPY' => array(
+        'name' => __( 'Japanese Yen', 'ninja-forms' ),
+        'value' => 'JPY'
+    ),
+
+    'MXN' => array(
+        'name' => __( 'Mexican Pesos', 'ninja-forms' ),
+        'value' => 'MXN'
+    ),
+
+    'NOK' => array(
+        'name' => __( 'Norwegian Krone', 'ninja-forms' ),
+        'value' => 'NOK'
+    ),
+
+    'NZD' => array(
+        'name' => __( 'New Zealand Dollars', 'ninja-forms' ),
+        'value' => 'NZD'
+    ),
+
+    'PHP' => array(
+        'name' => __( 'Philippine Pesos', 'ninja-forms' ),
+        'value' => 'PHP'
+    ),
+
+    'PLN' => array(
+        'name' => __( 'Polish Zloty', 'ninja-forms' ),
+        'value' => 'PLN'
+    ),
+
+    'GBP' => array(
+        'name' => __( 'Pound Sterling', 'ninja-forms' ),
+        'value' => 'GBP'
+    ),
+
+    'SGD' => array(
+        'name' => __( 'Singapore Dollars', 'ninja-forms' ),
+        'value' => 'SGD'
+    ),
+
+    'SEK' => array(
+        'name' => __( 'Swedish Krona', 'ninja-forms' ),
+        'value' => 'SEK'
+    ),
+
+    'CHF' => array(
+        'name' => __( 'Swiss Franc', 'ninja-forms' ),
+        'value' => 'CHF'
+    ),
+
+    'TWD' => array(
+        'name' => __( 'Taiwan New Dollars', 'ninja-forms' ),
+        'value' => 'TWD'
+    ),
+
+    'THB' => array(
+        'name' => __( 'Thai Baht', 'ninja-forms' ),
+        'value' => 'THB'
+    ),
+
+    'USD' => array(
+        'name' => __( 'U.S. Dollars', 'ninja-forms' ),
+        'value' => 'USD'
+    ),
+
+));
