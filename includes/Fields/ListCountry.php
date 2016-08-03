b8c6431583a850e9cebe2d38cf462cf1ae5c275e
Kyle B. Johnson <kyle@wpninjas.com>
2016-08-03 12:53:50 -0400

    add default value setting.

--- includes/Fields/ListCountry.php
+++ includes/Fields/ListCountry.php
@@ -21,7 +21,31 @@ class NF_Fields_ListCountry extends NF_Abstracts_List
 
         $this->_nicename = __( 'Country', 'ninja-forms' );
 
-        $this->_settings[ 'options' ][ 'value' ] = $this->get_options();
+        $this->_settings[ 'options' ][ 'group' ] = '';
+//        $this->_settings[ 'options' ][ 'value' ] = $this->get_options();
+
+        $this->_settings[ 'default' ] = array(
+            'name' => 'default',
+            'type' => 'select',
+            'label' => __( 'Default Value', 'ninja-forms' ),
+            'options' => $this->get_default_value_options(),
+            'width' => 'one-half',
+            'group' => 'primary',
+            'value' => 'US',
+        );
+    }
+
+    private function get_default_value_options()
+    {
+        $options = array();
+        foreach( Ninja_Forms()->config( 'CountryList' ) as $label => $value ){
+            $options[] = array(
+                'label'  => $label,
+                'value' => $value,
+            );
+        }
+
+        return $options;
     }
 
     private function get_options()
