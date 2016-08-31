804e3300389c7b64ec04ed8990346389fb5c3c6b
Kyle B. Johnson <kyle@wpninjas.com>
2016-08-31 14:37:47 -0400

    add support for shortcodes in success messages. Also add console logging.

--- assets/js/front-end/controllers/submitDebug.js
+++ assets/js/front-end/controllers/submitDebug.js
@@ -5,15 +5,27 @@ define([], function() {
         },
 
         submitDebug: function( response, textStatus, jqXHR, formID ) {
-            
-            if( 'undefined' == typeof response.debug ) return;
-            
-            var debugMessages = '';
-            _.each( response.debug, function( message, index ){
-                debugMessages += message + '<br />';
-            });
 
-            jQuery( '.nf-debug-msg' ).html( debugMessages );
+            if( 'undefined' == typeof response.debug ) return;
+
+            /* Form Debug Messages */
+            if( 'undefined' != typeof response.debug.form ) {
+                var debugMessages = '';
+                _.each(response.debug.form, function (message, index) {
+                    debugMessages += message + '<br />';
+                });
+                jQuery('.nf-debug-msg').html(debugMessages);
+            }
+
+            /* Console Debug Messages */
+            if( 'undefined' != typeof response.debug.console ) {
+                var style = '';
+                console.log( '%c%s', style, 'NINJA SUPPORT' );
+                _.each(response.debug.console, function (message, index) {
+                    console.log( message );
+                });
+                console.log( '%c%s', style, 'END NINJA SUPPORT' );
+            }
         }
 
     });
