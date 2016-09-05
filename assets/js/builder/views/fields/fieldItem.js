26f2be24c07bb5f377e224cde5fb47fe220df883
Kevin Stover <kstover@gmail.com>
2016-09-05 18:12:35 -0400

    Fixing a bug that caused form publishing to fail. Closes #1687. Also changed the updateDB error reporting to console.log the exception.

--- assets/js/builder/views/fields/fieldItem.js
+++ assets/js/builder/views/fields/fieldItem.js
@@ -9,14 +9,26 @@ define( ['views/app/itemControls'], function( itemControlsView ) {
 		},
 
 		initialize: function() {
-			this.listenTo( this.model, 'change:editActive', this.render, this );
-			this.listenTo( this.model, 'change:label', this.render, this );
-			this.listenTo( this.model, 'change:required', this.render, this );
-			this.listenTo( this.model, 'change:id', this.render, this );
+			this.model.on( 'change:editActive', this.render, this );
+			this.model.on( 'change:label', this.render, this );
+			this.model.on( 'change:required', this.render, this );
+			this.model.on( 'change:id', this.render, this );
+		},
+
+		onBeforeDestroy: function() {
+			this.model.off( 'change:editActive', this.render );
+			this.model.off( 'change:label', this.render );
+			this.model.off( 'change:required', this.render );
+			this.model.off( 'change:id', this.render );
 		},
 
 		onRender: function() {
+			this.$el = this.$el.children();
+			this.$el.unwrap();
+			this.setElement( this.$el );
+
 			this.itemControls.show( new itemControlsView( { model: this.model } ) );
+			jQuery( this.el ).disableSelection();
 
 			if ( nfRadio.channel( 'app' ).request( 'is:mobile' ) ) {
 				jQuery( this.el ).on( 'taphold', function( e, touch ) {
