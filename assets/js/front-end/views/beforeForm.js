f0368c3b882fbece88945fb9763f429f798f9020
Kyle B. Johnson <kyle@wpninjas.com>
2016-09-07 12:45:27 -0400

    add fields marked required message conditionally. Closes #1710.

--- assets/js/front-end/views/beforeForm.js
+++ assets/js/front-end/views/beforeForm.js
@@ -2,7 +2,18 @@ define( [], function( ) {
 
 	var view = Marionette.ItemView.extend({
 		tagName: "nf-section",
-		template: "#tmpl-nf-before-form"
+		template: "#tmpl-nf-before-form",
+
+		templateHelpers: function () {
+			return {
+
+				renderFieldsMarkedRequired: function() {
+					var requiredFields = _.filter( this.fields.models, function( field ) { return field.get( 'required' ) } );
+					return ( requiredFields.length ) ? nfi18n.fieldsMarkedRequired : '';
+				},
+			};
+		},
+
 	});
 
 	return view;
