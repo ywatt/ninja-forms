a1b780981ed74ee5d8863923fa39cc6ebce0ecb8
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2016-09-07 17:50:53 -0400

    Add missing Required Message (#1711)
    * upate field tempalte tags.
    
    * fix missing required asterisk.
    
    * add fields marked required message conditionally. Closes #1710.
    
    * refactor field filter.

--- assets/js/front-end/views/beforeForm.js
+++ assets/js/front-end/views/beforeForm.js
@@ -2,7 +2,18 @@ define( [], function( ) {
 
 	var view = Marionette.ItemView.extend({
 		tagName: "nf-section",
-		template: "#tmpl-nf-before-form"
+		template: "#tmpl-nf-before-form",
+
+		templateHelpers: function () {
+			return {
+
+				renderFieldsMarkedRequired: function() {
+					var requiredFields = this.fields.filter( { required: 1 } );
+					return ( requiredFields.length ) ? nfi18n.fieldsMarkedRequired : '';
+				},
+			};
+		},
+
 	});
 
 	return view;
