6d6d2ce0a5773d20dbab809ff72653a821b9c0b8
Kyle B. Johnson <kyle@wpninjas.com>
2016-10-18 11:29:39 -0400

    verison bump v3.0.10.

--- deprecated/ninja-forms.php
+++ deprecated/ninja-forms.php
@@ -265,7 +265,7 @@ class Ninja_Forms {
 
         // Plugin version
         if ( ! defined( 'NF_PLUGIN_VERSION' ) )
-            define( 'NF_PLUGIN_VERSION', '3.0.9' );
+            define( 'NF_PLUGIN_VERSION', '3.0.10' );
 
         // Plugin Folder Path
         if ( ! defined( 'NF_PLUGIN_DIR' ) )
