28c9be3a7c7e918c7e161568a712dd2170beba32
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2016-11-21 16:46:57 -0500

    add a default value if not submitted. (#2115)

--- includes/AJAX/Controllers/Submission.php
+++ includes/AJAX/Controllers/Submission.php
@@ -133,11 +133,14 @@ class NF_AJAX_Controllers_Submission extends NF_Abstracts_Controller
             $field_id = $field[ 'id' ];
 
             // Check that the field ID exists in the submitted for data and has a submitted value.
-            if( ! isset( $this->_form_data[ 'fields' ][ $field_id ] ) ) continue;
-            if( ! isset( $this->_form_data[ 'fields' ][ $field_id ][ 'value' ] ) ) continue;
+            if( isset( $this->_form_data[ 'fields' ][ $field_id ] ) && isset( $this->_form_data[ 'fields' ][ $field_id ][ 'value' ] ) ){
+                $field[ 'value' ] = $this->_form_data[ 'fields' ][ $field_id ][ 'value' ];
+            } else {
+                $field[ 'value' ] = '';
+            }
 
             // Duplicate field value to settings and top level array item for backwards compatible access (ie Save Action).
-            $field[ 'settings' ][ 'value' ] = $field[ 'value' ] = $this->_form_data[ 'fields' ][ $field_id ][ 'value' ];
+            $field[ 'settings' ][ 'value' ] = $field[ 'value' ];
 
             // Duplicate field value to form cache for passing to the action filter.
             $this->_form_cache[ 'fields' ][ $key ][ 'settings' ][ 'value' ] = $this->_form_data[ 'fields' ][ $field_id ][ 'value' ];
