6fa64008ef5d5cbc379f57465cbc13bf048dd3c4
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2017-01-26 13:32:11 -0500

    add better handling of non-UTF8 characters during import. Closes #2340. (#2341)

--- includes/Abstracts/ModelFactory.php
+++ includes/Abstracts/ModelFactory.php
@@ -128,6 +128,7 @@ class NF_Abstracts_ModelFactory
     public function import_form( $import, $id = FALSE, $is_conversion = FALSE )
     {
         if( ! is_array( $import ) ){
+
             $data = WPN_Helper::utf8_decode( json_decode( html_entity_decode( $import ), true ) );
 
             if( ! is_array( $data ) ) {
@@ -135,7 +136,7 @@ class NF_Abstracts_ModelFactory
             }
 
             if( ! is_array( $data ) ){
-                $data = maybe_unserialize( $import );
+                $data = WPN_Helper::maybe_unserialize( $import );
 
                 if( ! is_array( $data ) ){
                     return false;
