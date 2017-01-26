acb0de38da5b7586320c9193b368584c05ebe162
Kevin Stover <kevin@wpninjas.com>
2017-01-26 14:34:23 -0500

    Fixing a JS error that prevented tags from being added properly to Calculation. Closes #2353. (#2360)

--- assets/js/builder/views/app/drawer/optionRepeaterError.js
+++ assets/js/builder/views/app/drawer/optionRepeaterError.js
@@ -2,7 +2,20 @@ define( [], function() {
 	var view = Marionette.ItemView.extend({
 		tagName: 'div',
 		className: 'nf-error',
-		template: '#tmpl-nf-edit-setting-option-repeater-error'
+		template: '#tmpl-nf-edit-setting-option-repeater-error',
+
+		templateHelpers: function() {
+			var that = this;
+			return {
+				renderErrors: function() {
+				    if ( 'undefined' != typeof that.errors ) {
+    					return that.errors[ Object.keys( errors )[0] ];
+ 					} else {
+ 						return '';
+ 					}
+				}
+			}
+		}
 	});
 
 	return view;
