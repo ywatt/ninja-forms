4d9a5faf8cbff4c8507fbe74583b53fce68a8087
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2017-01-31 14:44:38 -0500

    use uasort to maintain key associations. Closes #2383. (#2384)

--- includes/Database/Models/Submission.php
+++ includes/Database/Models/Submission.php
@@ -308,7 +308,7 @@ final class NF_Database_Models_Submission
          * TODO: This is probably not the most effecient way to handle this. It should be re-thought.
          */
         if ( ! has_filter( 'ninja_forms_get_fields_sorted' ) ) {
-            usort( $fields, array( 'NF_Database_Models_Submission', 'sort_fields' ) );
+            uasort( $fields, array( 'NF_Database_Models_Submission', 'sort_fields' ) );
         }
 
         $hidden_field_types = apply_filters( 'nf_sub_hidden_field_types', array() );
