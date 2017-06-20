d18e9b13ab2688ac3ddefdc32fb1e64208c0d842
kjohnson <me@kylebjohnson.me>
2017-06-20 15:44:07 -0400

    add upgrader skin with no output.

---
+++ includes/Libraries/RemoteInstaller/PluginInstallerSkin.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * A custom skin with no output.
+ */
+class NF_Libraries_RemoteInstaller_PluginInstallerSkin extends Plugin_Installer_Skin {
+    public function error( $errors ){
+        // This secion intentionally left blank.
+    }
+    public function feedback( $string ){
+        // This section intentionally left blank.
+    }
+    public function before(){
+        // This section intentionally left blank.
+    }
+    public function after(){
+        // This section intentionally left blank.
+    }
+}
