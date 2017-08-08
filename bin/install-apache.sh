8cfafdcb7dae8219fe1560be9bfc6eee4dbe5cc9
Chris Klosowski <cklosowski@gmail.com>
2017-08-08 02:15:09 -0700

    Clear ip tables so port 80 works

--- bin/install-apache.sh
+++ bin/install-apache.sh
@@ -27,3 +27,16 @@ sudo a2enconf servername
 sudo service apache2 restart
 # Restart PHP
 sudo service php7.0-fpm restart
+
+# Flush the iptables
+echo "Flushing iptables rules..."
+sleep 1
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
