5a5bc74be7d6b00df7fa90e11f8fc60f94245985
Chris Klosowski <cklosowski@gmail.com>
2017-08-08 04:47:24 -0700

    Try firefox?

--- tests/acceptance/003-DisplayLoadCept.php
+++ tests/acceptance/003-DisplayLoadCept.php
@@ -7,4 +7,7 @@ $I->loginAsAdmin();
 $I->wantTo( 'confirm that form display loads properly' );
 $I->amOnPage( '/?nf_preview_form=1' );
 $I->waitForElementVisible( '.nf-form-content', 30 );
-$I->waitForElementVisible( 'nf-field-{{{ data.id }}}"', 30 );
+$I->waitForElementVisible( '#nf-field-1', 30 );
+$I->waitForElementVisible( '#nf-field-2', 30 );
+$I->waitForElementVisible( '#nf-field-3', 30 );
+$I->waitForElementVisible( '#nf-field-4', 30 );
