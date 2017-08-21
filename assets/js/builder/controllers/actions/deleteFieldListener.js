25146cdde0dbb4f33cd9191654a3517969453f9d
Kevin Stover <kstover@gmail.com>
2017-08-21 08:20:20 -0400

    Option repeaters should now properly remove options that include fields that have been deleted.

--- assets/js/builder/controllers/actions/deleteFieldListener.js
+++ assets/js/builder/controllers/actions/deleteFieldListener.js
@@ -20,23 +20,63 @@ define( [], function() {
 		},
 
 		maybeUpdateSettings: function( fieldModel ) {
-
+			var newObject, filteredCollection,
+				fieldMergeTag = '{field:' + fieldModel.get( 'key' ) + '}';
+			
+			/*
+			 * Loop through our action attributes to see if the field mergetag exists in our action.
+			 *
+			 * If it does:
+			 * 	- Replace the field mergetag in strings with ''.
+			 * 	- Remove any items with the field merge tag if they are in an array.
+			 */
 			_.each( this.attributes, function( attr, key ) {
-				var fieldMergeTag = '{field:' + fieldModel.get( 'key' ) + '}';
 				if ( _.isString( attr ) ) {
+					// If our attribute is a string, replace any instances of the field merge tag with an empty string.
 					this.set( key, attr.replace( fieldMergeTag, '' ) );
 				} else if ( _.isArray( attr ) ) {
-					_.each( attr, function( obj, index ) {
-						obj = _.mapObject( obj, function( val, k ) {
-							if ( _.isString( val ) ) {
-								if ( -1 != val.indexOf( fieldMergeTag ) ) {
-									attr.splice( index, 1 );
-									this.set( key, attr );
-								}
-							};
-							return val;
-						}, this );
+					// If our attribute is an array, search the contents for field merge tag and remove items that match.
+					_.each( attr, function( val, index ) {
+						if ( _.isString( val ) ) {
+							// If val is a string, search it for the field mergetag.
+							console.log( 'string replace' );
+						} else if ( _.isArray( val ) ) {
+							// If val is an array, search it for the field mergetag.
+							console.log( 'array search' );
+						} else if ( _.isObject( val ) ) {
+							// If val is a object, search it for the field mergetag.
+							newObject = _.mapObject( val, function( value, key ) {
+								if ( _.isString( value ) ) {
+									if ( -1 != value.indexOf( fieldMergeTag ) ) {
+										attr.splice( index, 1 );
+									}
+								};
+
+								return value;
+							} );
+
+							this.set( key, attr );
+						}
 					}, this );
+				} else if ( attr instanceof Backbone.Collection ) {
+					// This is a Backbone Collection, so we need to loop through the models and remove any that have an attribute containing the field merge tag.
+					var filteredCollection = attr.filter( function ( model ) {
+						// Make sure that EVERY model attribute does NOT reference the field merge tag.
+					    return _.every( model.attributes, function( val ) {
+					    	/*
+					    	 * Currently only handles items that are one-level deep.
+					    	 * TODO: Add support for further nesting of values.
+					    	 */
+					    	if ( _.isString( val ) ) {
+					    		if ( -1 != val.indexOf( fieldMergeTag ) ) {
+					    			return false;
+					    		}
+					    	}
+					    	return true;
+					    });;
+					});
+					// Update our key with the filtered collection value.
+					this.set( key, filteredCollection );
 				}
 			}, this );
 		}
