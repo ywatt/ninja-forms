465215dbd837557fbd1bc5145041372360068998
kjohnson <me@kylebjohnson.me>
2017-09-01 20:33:15 -0400

    add throttled check to THREE upgrade notice.

---
+++ lib/NF_UpgradeThrottle.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Upgrade Throttle
+ * @param int $threshold
+ * @option int $compare
+ * @return bool
+ */
+function ninja_forms_three_throttle( $threshold = 1 ) {
+    $compare = get_option( 'ninja_forms_three_throttle', 0 );
+    if( ! $compare ){
+        $compare = rand( 1, 10 );
+//        update_option( 'ninja_forms_three_throttle', $compare );
+    }
+    return ( $threshold >= $compare );
+}
