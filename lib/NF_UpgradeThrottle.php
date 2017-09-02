63c181eedad3efcf93148c1aa4d51651a83bc627
kjohnson <me@kylebjohnson.me>
2017-09-01 20:38:08 -0400

    store generated number as an option.

--- lib/NF_UpgradeThrottle.php
+++ lib/NF_UpgradeThrottle.php
@@ -10,7 +10,7 @@ function ninja_forms_three_throttle( $threshold = 1 ) {
     $compare = get_option( 'ninja_forms_three_throttle', 0 );
     if( ! $compare ){
         $compare = rand( 1, 10 );
-//        update_option( 'ninja_forms_three_throttle', $compare );
+        update_option( 'ninja_forms_three_throttle', $compare );
     }
     return ( $threshold >= $compare );
 }
