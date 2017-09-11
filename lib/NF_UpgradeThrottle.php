b3e1b455897b62d812ddde5f9e76ec029ccce432
kjohnson <me@kylebjohnson.me>
2017-09-11 12:01:21 -0400

    upgrade throttle to ~5% of 100.

--- lib/NF_UpgradeThrottle.php
+++ lib/NF_UpgradeThrottle.php
@@ -6,10 +6,10 @@
  * @option int $compare
  * @return bool
  */
-function ninja_forms_three_throttle( $threshold = 1 ) {
+function ninja_forms_three_throttle( $threshold = 5 ) {
     $compare = get_option( 'ninja_forms_three_throttle', 0 );
     if( ! $compare ){
-        $compare = rand( 1, 10 );
+        $compare = rand( 1, 100 );
         update_option( 'ninja_forms_three_throttle', $compare );
     }
     return ( $threshold >= $compare );
