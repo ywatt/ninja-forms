6a13c088ac13fbdaf9d54be6f777be09dba3449c
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2017-09-20 13:48:14 -0400

    Removed extra spacing for comment alignment.

--- includes/Actions/Email.php
+++ includes/Actions/Email.php
@@ -142,7 +142,7 @@ final class NF_Actions_Email extends NF_Abstracts_Action
             // Loop over our email addresses.
             foreach( $email_addresses as $email ) {
 
-				// Updated to trim values in case there is a value with spaces/tabs/etc to remove whitespace
+		// Updated to trim values in case there is a value with spaces/tabs/etc to remove whitespace
                 if( empty( trim( $email ) ) ) continue;
 
                 // Build our array of the email addresses.
