58cf2518cc3460b6835fcf369c5b7cf17703297c
Eric Windham <eric.windham@gmail.com>
2018-01-12 14:45:16 -0500

    fixes issue#3289 where optionRepeaters were not setting the selected option correctly (#3290)

--- assets/js/builder/views/app/drawer/optionRepeaterOption.js
+++ assets/js/builder/views/app/drawer/optionRepeaterOption.js
@@ -160,13 +160,14 @@ define( ['views/app/drawer/optionRepeaterError'], function( ErrorView ) {
 					if( 'undefined' == typeof that.options.columns[ column ] ) return;
 
 					var select = document.createElement( 'select' );
-
-					var html = '';
+					
 					_.each( that.options.columns[ column ].options, function( option ){
 						var optionNode = document.createElement( 'option' );
-                        optionNode.selected = ( value == option.value );
-                        optionNode.value = option.value;
-                        optionNode.label = option.label;
+                        if ( value === option.value ) {
+                        	optionNode.setAttribute( 'selected', 'selected' );
+                        }
+                        optionNode.setAttribute( 'value', option.value );
+                        optionNode.setAttribute( 'label', option.label );
                         select.appendChild( optionNode );
 					});
 
